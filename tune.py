f91e4e9638b300894cfea678d6ec66574f353a03
Daniel Hershcovich <danielh@cs.huji.ac.il>
2016-05-03 17:52:50 +0300

    Simplify parameter tuning to avoid repetition

--- tune.py
+++ tune.py
@@ -7,18 +7,17 @@ from parsing.config import Config
 from ucca.evaluation import Scores
 
 
-class Hyperparams(object):
-    def __init__(self, learning_rate, decay_factor):
-        self.learning_rate = learning_rate
-        self.decay_factor = decay_factor
+class Params(object):
+    def __init__(self, **kwargs):
+        self.params = kwargs
         self.scores = None
 
     def run(self):
         assert Config().args.train and Config().args.passages or Config().args.folds, \
             "insufficient parameters given to parser"
         print("Running with %s" % self)
-        Config().learning_rate = self.learning_rate
-        Config().decay_factor = self.decay_factor
+        for name, value in self.params.items():
+            setattr(Config(), name, value)
         self.scores = parse.main()
         assert self.score is not None, "parser failed to produce score"
 
@@ -26,45 +25,41 @@ class Hyperparams(object):
         return -float("inf") if self.scores is None else self.scores.average_unlabeled_f1()
 
     def __str__(self):
-        ret = "learning rate: %.3f" % self.learning_rate
-        ret += ", decay factor: %.3f" % self.decay_factor
+        ret = ", ".join("%s: %s" % (name, value) for name, value in self.params.items())
         if self.scores is not None:
             ret += ", average unlabeled f1: %.3f" % self.score()
         return ret
 
     def print(self, file):
-        print(", ".join(["%.3f" % float(p) for p in
-                         (self.learning_rate, self.decay_factor, self.score())] +
-                        self.scores.fields()),
+        print(", ".join(list(map(str, self.params.values())) + [str(self.score())] + self.scores.fields()),
               file=file)
 
-    @staticmethod
-    def print_title(file):
-        print("learning rate, decay factor, average unlabeled f1, "
-              ", ".join(Scores.field_titles()),
+    def print_title(self, file):
+        print(", ".join(list(self.params) + ["average unlabeled f1"] + Scores.field_titles()),
               file=file)
 
 
 def main():  # TODO load passage files only once
-    out_file = os.environ.get("HYPERPARAMS_FILE", "hyperparams.csv")
-    num = int(os.environ.get("HYPERPARAMS_NUM", 30))
-    dims = (num, 2)
-    hyperparams = list(set(
-        Hyperparams(learning_rate, decay_factor)
-        for learning_rate, decay_factor in
-        np.round(0.001 + np.random.exponential(0.8, dims), 3)
-    ))
-    print("\n".join(["All hyperparam combinations to try: "] +
-                    [str(h) for h in hyperparams]))
+    out_file = os.environ.get("PARAMS_FILE", "params.csv")
+    num = int(os.environ.get("PARAMS_NUM", 30))
+    param_values = {
+        "learning_rate": np.round(0.001 + np.random.exponential(0.8, (num, 1)), 3),
+        "decay_factor": np.round(0.001 + np.random.exponential(0.8, (num, 1)), 3),
+    }
+    params = list(set(Params(**{name: values[i]
+                                for name, values in param_values.items()})
+                      for i in range(num)))
+    print("\n".join(["All parameter combinations to try: "] +
+                    [str(h) for h in params]))
     print("Saving results to '%s'" % out_file)
     with open(out_file, "w") as f:
-        Hyperparams.print_title(f)
-    for hyperparam in hyperparams:
-        hyperparam.run()
+        params[0].print_title(f)
+    for param in params:
+        param.run()
         with open(out_file, "a") as f:
-            hyperparam.print(f)
-        best = max(hyperparams, key=Hyperparams.score)
-        print("Best hyperparams: %s" % best)
+            param.print(f)
+        best = max(params, key=Params.score)
+        print("Best parameters: %s" % best)
 
 
 if __name__ == "__main__":
