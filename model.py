a03ce75e8169b6158137c2e39ec894df35ea780a
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2016-05-11 14:43:26 +0300

    Rename variables and modules to avoid import errors

--- model.py
+++ model.py
@@ -4,29 +4,29 @@ from parsing.config import Config
 
 
 class Model(object):
-    def __init__(self, model_type=None, labels=None, features=None, model=None):
+    def __init__(self, model_type=None, labels=None, feature_extractor=None, model=None):
         self._update_only_on_error = None
         self.model_type = model_type
-        if features is not None and model is not None:
-            self.features = features
+        if feature_extractor is not None and model is not None:
+            self.feature_extractor = feature_extractor
             self.model = model
             return
 
         if model_type == config.SPARSE_PERCEPTRON:
             from classifiers.sparse_perceptron import SparsePerceptron
             from features.sparse_features import SparseFeatureExtractor
-            self.features = SparseFeatureExtractor()
+            self.feature_extractor = SparseFeatureExtractor()
             self.model = SparsePerceptron(labels, min_update=Config().args.minupdate)
         elif model_type == config.DENSE_PERCEPTRON:
             from features.embedding import FeatureEmbedding
             from classifiers.dense_perceptron import DensePerceptron
-            self.features = self.dense_features_wrapper(FeatureEmbedding)
-            self.model = DensePerceptron(labels, num_features=self.features.num_features())
+            self.feature_extractor = self.dense_features_wrapper(FeatureEmbedding)
+            self.model = DensePerceptron(labels, num_features=self.feature_extractor.num_features())
         elif model_type == config.NEURAL_NETWORK:
             from features.indexer import FeatureIndexer
             from classifiers.neural_network import NeuralNetwork
-            self.features = self.dense_features_wrapper(FeatureIndexer)
-            self.model = NeuralNetwork(labels, inputs=self.features.feature_types,
+            self.feature_extractor = self.dense_features_wrapper(FeatureIndexer)
+            self.model = NeuralNetwork(labels, inputs=self.feature_extractor.feature_types,
                                        layers=Config().args.layers,
                                        layer_dim=Config().args.layerdim,
                                        activation=Config().args.activation,
@@ -53,7 +53,7 @@ class Model(object):
                        )
 
     def extract_features(self, *args, **kwargs):
-        return self.features.extract_features(*args, **kwargs)
+        return self.feature_extractor.extract_features(*args, **kwargs)
 
     def score(self, *args, **kwargs):
         return self.model.score(*args, **kwargs)
@@ -69,14 +69,14 @@ class Model(object):
 
     def finalize(self, *args, **kwargs):
         return Model(model_type=self.model_type,
-                     features=self.features.finalize(*args, **kwargs),
+                     feature_extractor=self.feature_extractor.finalize(*args, **kwargs),
                      model=self.model.finalize(*args, **kwargs))
 
     def save(self, *args, **kwargs):
-        self.features.save(*args, **kwargs)
+        self.feature_extractor.save(*args, **kwargs)
         self.model.save(*args, **kwargs)
 
     def load(self, *args, **kwargs):
-        self.features.load(*args, **kwargs)
+        self.feature_extractor.load(*args, **kwargs)
         self.model.load(*args, **kwargs)
         Actions().all = self.model.labels
