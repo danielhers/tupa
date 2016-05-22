15cf25a621fdb0383654170ad50f88d6b9d429aa
Daniel Hershcovich <danielh@cs.huji.ac.il>
2016-05-22 16:54:53 +0300

    Allow loading binary word2vec file

--- w2v_util.py
+++ w2v_util.py
@@ -4,5 +4,8 @@ from gensim.models.word2vec import Word2Vec
 def load_word2vec(w2v):
     if isinstance(w2v, str):
         print("Loading word vectors from '%s'..." % w2v, flush=True)
-        w2v = Word2Vec.load_word2vec_format(w2v)
+        try:
+            w2v = Word2Vec.load_word2vec_format(w2v)
+        except ValueError:
+            w2v = Word2Vec.load_word2vec_format(w2v, binary=True)
     return w2v
