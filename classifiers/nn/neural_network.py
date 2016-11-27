shachardon/ucca

Get hyperparameters directly from config; refactor perceptron classes

parsing/classifiers/nn/neural_network.py
@@ -7,7 +7,7 @@ from collections import OrderedDict
 
 import dynet as dy
 from classifiers.classifier import Classifier
-from parsing import config
+from parsing.config import Config
 from parsing.model_util import load_dict, save_dict
 
 TRAINERS = {
@@ -42,37 +42,25 @@ class NeuralNetwork(Classifier):
     Expects features from FeatureEnumerator.
     """
 
-    def __init__(self, filename, labels, model_type, input_params=None,
-                 layers=1, layer_dim=100, activation="tanh",
-                 init="glorot_uniform", max_num_labels=100,
-                 minibatch_size=200, dropout=0, optimizer="adam"):
+    def __init__(self, *args, input_params):
         """
-        Create a new untrained NN or copy the weights from an existing one
+        Create a new untrained NN
         :param labels: a list of labels that can be updated later to add a new label
         :param input_params: dict of feature type name -> FeatureInformation
-        :param layers: number of hidden layers
-        :param layer_dim: size of hidden layer
-        :param activation: activation function at hidden layers
-        :param init: initialization type for hidden layers
-        :param max_num_labels: since model size is fixed, set maximum output size
-        :param minibatch_size: batch size for training
-        :param dropout: dropout to apply to each layer
-        :param optimizer: algorithm to use for optimization
         """
-        super(NeuralNetwork, self).__init__(model_type=model_type, filename=filename, labels=labels)
-        assert input_params is not None
-        self.max_num_labels = max_num_labels
-        self._layers = layers
-        self._layer_dim = layer_dim
-        self._activation_str = activation
+        super(NeuralNetwork, self).__init__(*args)
+        self.max_num_labels = Config().args.maxlabels
+        self._layers = Config().args.layers
+        self._layer_dim = Config().args.layerdim
+        self._activation_str = Config().args.activation
+        self._init_str = Config().args.init
+        self._minibatch_size = Config().args.minibatchsize
+        self._dropout = Config().args.dropout
+        self._optimizer_str = Config().args.optimizer
         self._activation = ACTIVATIONS[self._activation_str]
-        self._init_str = init
         self._init = INITIALIZERS[self._init_str]
-        self._num_labels = self.num_labels
-        self._minibatch_size = minibatch_size
-        self._dropout = dropout
-        self._optimizer_str = optimizer
         self._optimizer = TRAINERS[self._optimizer_str]
+        self._num_labels = self.num_labels
         self._params = OrderedDict()
         self._input_params = input_params
         self._losses = []
@@ -87,15 +75,12 @@ class NeuralNetwork(Classifier):
     def resize(self):
         assert self.num_labels <= self.max_num_labels, "Exceeded maximum number of labels"
 
-    def evaluate(self, features):
-        raise NotImplementedError
-
-    def generate_inputs(self, features):
-        raise NotImplementedError
+    def evaluate(self, features, train=False):
+        raise NotImplementedError()
 
     def init_model(self):
         self.model = dy.Model()
-        self._trainer = self._optimizer(self.model)
+        self._trainer = self._optimizer(self.model, )
         input_dim = self.init_inputs()
         self.init_mlp(input_dim)
 
@@ -171,11 +156,11 @@ class NeuralNetwork(Classifier):
             self._losses.append(dy.pick(self.evaluate(features, train=True), true))
             if len(self._losses) >= self._minibatch_size:
                 self.finalize()
-            if config.Config().args.dynet_viz:
+            if Config().args.dynet_viz:
                 dy.print_graphviz()
                 sys.exit(0)
 
-    def finalize(self):
+    def finalize(self, finished_epoch=False):
         """
         Fit this model on collected samples
         :return self
@@ -188,6 +173,8 @@ class NeuralNetwork(Classifier):
             self._trainer.update()
             self._losses = []
             self._iteration += 1
+        if finished_epoch:
+            self._trainer.update_epoch()
         return self
 
     def save(self):
@@ -229,7 +216,6 @@ class NeuralNetwork(Classifier):
     def load(self):
         """
         Load all parameters from file
-        :param suffix: extra suffix to append to filename
         """
         self.init_model()
         d = load_dict(self.filename)
@@ -253,9 +239,9 @@ class NeuralNetwork(Classifier):
         try:
             param_values = self.model.load(model_filename)
             print("Done (%.3fs)." % (time.time() - started))
+            self._params = OrderedDict(zip(param_keys, param_values))
         except KeyError as e:
             print("Failed loading model: %s" % e)
-        self._params = OrderedDict(zip(param_keys, param_values))
 
     def load_extra(self, d):
         pass

parsing/model.py
@@ -15,26 +15,26 @@ class Model(object):
             return
 
         if model_type == config.SPARSE_PERCEPTRON:
-            from classifiers.sparse_perceptron import SparsePerceptron
             from features.sparse_features import SparseFeatureExtractor
+            from linear.sparse_perceptron import SparsePerceptron
             self.feature_extractor = SparseFeatureExtractor()
-            self.model = SparsePerceptron(filename, labels, min_update=Config().args.minupdate)
+            self.model = SparsePerceptron(filename, labels)
         elif model_type == config.DENSE_PERCEPTRON:
             from features.embedding import FeatureEmbedding
-            from classifiers.dense_perceptron import DensePerceptron
+            from linear.dense_perceptron import DensePerceptron
             self.feature_extractor = self.dense_features_wrapper(FeatureEmbedding)
             self.model = DensePerceptron(filename, labels, num_features=self.feature_extractor.num_features())
         elif model_type == config.MLP_NN:
             from features.enumerator import FeatureEnumerator
             from nn.feedforward import MLP
             self.feature_extractor = self.dense_features_wrapper(FeatureEnumerator)
-            self.model = MLP(filename, labels, **self.nn_kwargs())
+            self.model = MLP(filename, labels, input_params=self.feature_extractor.params)
         elif model_type == config.BILSTM_NN:
             from features.enumerator import FeatureEnumerator
             from features.indexer import FeatureIndexer
             from nn.bilstm import BiLSTM
             self.feature_extractor = FeatureIndexer(self.dense_features_wrapper(FeatureEnumerator))
-            self.model = BiLSTM(filename, labels, **self.nn_kwargs())
+            self.model = BiLSTM(filename, labels, input_params=self.feature_extractor.params)
         else:
             raise ValueError("Invalid model type: '%s'" % model_type)
 
@@ -50,19 +50,6 @@ class Model(object):
             FeatureParameters("A", Config().args.actiondim, Config().args.maxactions),
         ]
         return wrapper(DenseFeatureExtractor(), params)
-
-    def nn_kwargs(self):
-        return {
-            "input_params": self.feature_extractor.params,
-            "layers": Config().args.layers,
-            "layer_dim": Config().args.layerdim,
-            "activation": Config().args.activation,
-            "init": Config().args.init,
-            "max_num_labels": Config().args.maxlabels,
-            "minibatch_size": Config().args.minibatchsize,
-            "dropout": Config().args.dropout,
-            "optimizer": Config().args.optimizer
-        }
 
     def extract_features(self, *args, **kwargs):
         return self.feature_extractor.extract_features(*args, **kwargs)
@@ -90,11 +77,11 @@ class Model(object):
             self._update_only_on_error = self.model_type in (config.SPARSE_PERCEPTRON, config.DENSE_PERCEPTRON)
         return self._update_only_on_error
 
-    def finalize(self, *args, **kwargs):
+    def finalize(self, finished_epoch):
         return Model(model_type=self.model_type,
                      filename=self.filename,
-                     feature_extractor=self.feature_extractor.finalize(*args, **kwargs),
-                     model=self.model.finalize(*args, **kwargs))
+                     feature_extractor=self.feature_extractor.finalize(),
+                     model=self.model.finalize(finished_epoch))
 
     def save(self):
         if self.filename is not None:

danielhers/tupa

classifiers/nn/neural_network.py
@@ -3,6 +3,7 @@ from keras import regularizers
 from keras.models import model_from_json
 
 from classifiers.classifier import Classifier
+from parsing.config import Config
 from parsing.model_util import load_dict, save_dict
 
 
@@ -14,56 +15,35 @@ class NeuralNetwork(Classifier):
     Expects features from FeatureEnumerator.
     """
 
-    def __init__(self, filename, labels, model_type, feature_params=None, model=None,
-                 layers=1, layer_dim=100, activation="tanh", normalize=False,
-                 init="glorot_normal", max_num_labels=100, batch_size=10,
-                 minibatch_size=200, nb_epochs=5, dropout=0,
-                 optimizer="adam", loss="categorical_crossentropy",
-                 regularizer="l2", regularization=1e-8):
+    def __init__(self, *args, input_params):
         """
-        Create a new untrained NN or copy the weights from an existing one
-        :param labels: a list of labels that can be updated later to add a new label
-        :param feature_params: dict of feature type name -> FeatureInformation
-        :param model: if given, copy the weights (from a trained model)
-        :param layers: number of hidden layers
-        :param layer_dim: size of hidden layer
-        :param activation: activation function at hidden layers
-        :param normalize: perform batch normalization after each layer?
-        :param init: initialization type for hidden layers
-        :param max_num_labels: since model size is fixed, set maximum output size
-        :param batch_size: fit model every this many items
-        :param minibatch_size: batch size for SGD
-        :param nb_epochs: number of epochs for SGD
-        :param dropout: dropout to apply to input layer
-        :param optimizer: algorithm to use for optimization
-        :param loss: objective function to use for optimization
-        :param regularizer: regularization type (None, l1, l2 or l1l2)
-        :param regularization: regularization parameter lambda
+        Create a new untrained NN
+        :param input_params: dict of feature type name -> FeatureInformation
         """
         super(NeuralNetwork, self).__init__(model_type=model_type, filename=filename,
                                             labels=labels, model=model)
-        assert feature_params is not None or model is not None
+        assert input_params is not None or model is not None
         if self.is_frozen:
             self.model = model
         else:
-            self.max_num_labels = max_num_labels
-            self._layers = layers
-            self._layer_dim = layer_dim
-            self._activation = (lambda x: x*x*x) if activation == "cube" else activation
-            self._normalize = normalize
-            self._init = init
+            self.max_num_labels = Config().args.maxlabels
+            self._layers = Config().args.layers
+            self._layer_dim = Config().args.layerdim
+            self._activation = (lambda x: x*x*x) if Config().args.activation == "cube" else Config().args.activation
+            self._normalize = Config().args.normalize
+            self._init = Config().args.init
             self._num_labels = self.num_labels
-            self._minibatch_size = minibatch_size
-            self._nb_epochs = nb_epochs
-            self._dropout = dropout
-            self._optimizer = optimizer
-            self._loss = (lambda t, p: K.sum(K.maximum(0., 1.-p*t+p*(1.-t)))) if loss == "max_margin" else loss
-            self._regularizer = (lambda: None) if regularizer is None else \
-                (lambda: regularizers.l1l2(regularization, regularization)) if regularizer == "l1l2" else \
-                (lambda: regularizers.get(regularizer, {"l": regularization}))
-            self.feature_params = feature_params
+            self._minibatch_size = Config().args.minibatchsize
+            self._nb_epochs = Config().args.nbepochs
+            self._dropout = Config().args.dropout
+            self._optimizer = Config().args.optimizer
+            self._loss = (lambda t, p: K.sum(K.maximum(0., 1.-p*t+p*(1.-t)))) if Config().args.loss == "max_margin" else Config().args.loss
+            self._regularizer = (lambda: None) if Config().args.regularizer is None else \
+                (lambda: regularizers.l1l2(Config().args.regularization, Config().args.regularization)) if Config().args.regularizer == "l1l2" else \
+                (lambda: regularizers.get(Config().args.regularizer, {"l": Config().args.regularization}))
+            self.input_params = input_params
             self.model = None
-        self._batch_size = batch_size
+        self._batch_size = Config().args.batchsize
         self._item_index = 0
         self._iteration = 0
 
@@ -75,7 +55,7 @@ class NeuralNetwork(Classifier):
 
     @property
     def input_dim(self):
-        return sum(f.num * f.dim for f in self.feature_params.values())
+        return sum(f.num * f.dim for f in self.input_params.values())
 
     def resize(self):
         assert self.num_labels <= self.max_num_labels, "Exceeded maximum number of labels"
@@ -102,7 +82,6 @@ class NeuralNetwork(Classifier):
     def load(self):
         """
         Load all parameters from file
-        :param filename: file to load from
         """
         d = load_dict(self.filename)
         model_type = d.get("type")

model.py
@@ -15,35 +15,20 @@ class Model(object):
             return
 
         if model_type == config.SPARSE_PERCEPTRON:
-            from classifiers.sparse_perceptron import SparsePerceptron
             from features.sparse_features import SparseFeatureExtractor
+            from linear.sparse_perceptron import SparsePerceptron
             self.feature_extractor = SparseFeatureExtractor()
-            self.model = SparsePerceptron(filename, labels, min_update=Config().args.minupdate)
+            self.model = SparsePerceptron(filename, labels)
         elif model_type == config.DENSE_PERCEPTRON:
             from features.embedding import FeatureEmbedding
-            from classifiers.dense_perceptron import DensePerceptron
+            from linear.dense_perceptron import DensePerceptron
             self.feature_extractor = self.dense_features_wrapper(FeatureEmbedding)
             self.model = DensePerceptron(filename, labels, num_features=self.feature_extractor.num_features())
         elif model_type == config.FEEDFORWARD_NN:
             from features.enumerator import FeatureEnumerator
             from nn.feedforward import FeedforwardNeuralNetwork
             self.feature_extractor = self.dense_features_wrapper(FeatureEnumerator)
-            self.model = FeedforwardNeuralNetwork(filename, labels, feature_params=self.feature_extractor.params,
-                                                  layers=Config().args.layers,
-                                                  layer_dim=Config().args.layerdim,
-                                                  activation=Config().args.activation,
-                                                  normalize=Config().args.normalize,
-                                                  init=Config().args.init,
-                                                  max_num_labels=Config().args.maxlabels,
-                                                  batch_size=Config().args.batchsize,
-                                                  minibatch_size=Config().args.minibatchsize,
-                                                  nb_epochs=Config().args.nbepochs,
-                                                  dropout=Config().args.dropout,
-                                                  optimizer=Config().args.optimizer,
-                                                  loss=Config().args.loss,
-                                                  regularizer=Config().args.regularizer,
-                                                  regularization=Config().args.regularization
-                                                  )
+            self.model = FeedforwardNeuralNetwork(filename, labels, input_params=self.feature_extractor.params)
         else:
             raise ValueError("Invalid model type: '%s'" % model_type)
 
@@ -81,11 +66,11 @@ class Model(object):
             self._update_only_on_error = self.model_type in (config.SPARSE_PERCEPTRON, config.DENSE_PERCEPTRON)
         return self._update_only_on_error
 
-    def finalize(self, *args, **kwargs):
+    def finalize(self, finished_epoch):
         return Model(model_type=self.model_type,
                      filename=self.filename,
-                     feature_extractor=self.feature_extractor.finalize(*args, **kwargs),
-                     model=self.model.finalize(*args, **kwargs))
+                     feature_extractor=self.feature_extractor.finalize(),
+                     model=self.model.finalize(finished_epoch))
 
     def save(self):
         if self.filename is not None: