09ba26b56fd7207432065a6d7173267f2a1f78b1
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-02-22 12:31:11 +0200

    Extract constants, fix handling of alignments, remove function nodes

--- amr/convert.py
+++ amr/convert.py
@@ -3,14 +3,25 @@ import re
 import amrutil
 from ucca import core, layer0, layer1
 from ucca.convert import FormatConverter
-from ucca.layer1 import EdgeTags
+
+COMMENT_PREFIX = "#"
+ID_PATTERN = "#\s*::id\s+(\S+)"
+TOK_PATTERN = "#\s*::(?:tok|snt)\s+(.*)"
+DEP_PREFIX = ":"
+TOP_DEP = "top"
+INSTANCE_DEP = "instance"
+INSTANCE_OF_DEP = "instance-of"
+ALIGNMENT_PREFIX = "e."
+ALIGNMENT_SEP = ","
+NODE_DEP_ATTRIB = "dep"
+TERMINAL_EDGE_TAG = "TERMINAL"
 
 
 class AmrConverter(FormatConverter):
     def __init__(self):
-        self.passage_id = self.amr_id = self.lines = self.tokens = self.AMR = self.return_amr = None
+        self.passage_id = self.amr_id = self.lines = self.tokens = self.return_amr = None
 
-    def from_format(self, lines, passage_id, return_amr, **kwargs):
+    def from_format(self, lines, passage_id, return_amr=False, **kwargs):
         del kwargs
         self.passage_id = passage_id
         self.return_amr = return_amr
@@ -19,14 +30,14 @@ class AmrConverter(FormatConverter):
         for line in lines:
             line = line.lstrip()
             if line:
-                if line[0] != "#":
+                if line[0] != COMMENT_PREFIX:
                     self.lines.append(line)
                     continue
-                m = re.match("#\s*::id\s+(\S+)", line)
+                m = re.match(ID_PATTERN, line)
                 if m:
                     self.amr_id = m.group(1)
                 else:
-                    m = re.match("#\s*::(?:tok|snt)\s+(.*)", line)
+                    m = re.match(TOK_PATTERN, line)
                     if m:
                         self.tokens = m.group(1).split()
             if self.lines:
@@ -41,34 +52,29 @@ class AmrConverter(FormatConverter):
         l1 = layer1.Layer1(p)
         self.lines = []
         self.amr_id = self.tokens = None
-        pending = amr.triples(rel=":top")
+        pending = amr.triples(rel=DEP_PREFIX + TOP_DEP)
         nodes = {}
         while pending:  # add normal nodes
             head, rel, dep = pending.pop()
-            rel = rel.lstrip(":")
+            rel = rel.lstrip(DEP_PREFIX)
             dependents = amr.triples(head=dep)
             pending += dependents
             if dep in nodes:  # reentrancy
                 l1.add_remote(nodes[head], rel, nodes[dep])
             else:
                 node = l1.add_fnode(nodes.get(head), rel, implicit=dep not in amr.alignments() and not dependents)
-                node.tag = repr(dep)
+                node.attrib[NODE_DEP_ATTRIB] = repr(dep)
                 nodes[dep] = node
         if amr.tokens():
             reverse_alignments = [None] * len(amr.tokens())
             for k, v in amr.alignments().items():
-                for i in v.lstrip("e.").split(","):  # remove prefix and separate by comma
+                for i in v.lstrip(ALIGNMENT_PREFIX).split(ALIGNMENT_SEP):  # remove prefix and separate by comma
                     reverse_alignments[int(i)] = k
             for i, token in enumerate(amr.tokens()):  # add terminals
                 triple = reverse_alignments[i]
-                if triple is None:  # unaligned token
-                    parent = l1.add_fnode(None, EdgeTags.Function)
-                elif isinstance(triple, tuple):
-                    parent = nodes[triple[0]]
-                else:
-                    parent = triple
-                parent.add(EdgeTags.Terminal, l0.add_terminal(text=token, punct=False))
-        return (p, amr, self.amr_id) if self.return_amr else p
+                parent = l1.top_node if triple is None else nodes[triple[0] if isinstance(triple, tuple) else triple]
+                parent.add(TERMINAL_EDGE_TAG, l0.add_terminal(text=token, punct=False))
+        return (p, amr(alignments=False), self.amr_id) if self.return_amr else p
 
     def to_format(self, passage, **kwargs):
         del kwargs
@@ -78,16 +84,17 @@ class AmrConverter(FormatConverter):
     @staticmethod
     def _to_triples(passage):
         def _node_string(node):
-            m = re.match("\w+\((.*)\)", node.tag)
-            return m.group(1) if m else node.tag
+            dep = node.attrib[NODE_DEP_ATTRIB]
+            m = re.match("\w+\((.*)\)", dep)
+            return m.group(1) if m else dep
 
         pending = list(passage.layer(layer1.LAYER_ID).top_node.outgoing)
         while pending:
             edge = pending.pop()
-            if edge.tag != EdgeTags.Function:  # skip function nodes
+            if edge.tag != TERMINAL_EDGE_TAG:  # skip function nodes
                 pending += edge.child.outgoing
-                if edge.tag != "top":  # omit top node from output
-                    tag = "instance" if edge.tag == "instance-of" else edge.tag
+                if edge.tag != TOP_DEP:  # omit top node from output
+                    tag = INSTANCE_DEP if edge.tag == INSTANCE_OF_DEP else edge.tag
                     yield _node_string(edge.parent), tag, _node_string(edge.child)
 
 
@@ -96,7 +103,7 @@ def from_amr(lines, passage_id=None, return_amr=False, *args, **kwargs):
 
     :param lines: iterable of lines in AMR PENMAN format, describing a single passage.
     :param passage_id: ID to set for passage, overriding the ID from the file
-    :param return_amr: return triple of (UCCA passage, AMR object, AMR ID)
+    :param return_amr: return triple of (UCCA passage, AMR string, AMR ID)
 
     :return generator of Passage objects
     """
