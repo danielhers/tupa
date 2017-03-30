f76e50d5eabbb2ff5c2963335a9413da3978d071
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-03-30 13:54:34 +0300

    Revert "Fix DyNet installation script Eigen path"
    This reverts commit 5dd3638d41e75a4437062b2f4fc5bb5caabe08da.

--- ci/install-dynet.sh
+++ ci/install-dynet.sh
@@ -3,9 +3,9 @@
 export CXX="g++-4.8" CC="gcc-4.8"
 git clone https://github.com/clab/dynet
 cd dynet
+hg clone https://bitbucket.org/eigen/eigen
 mkdir build
 cd build
-hg clone https://bitbucket.org/eigen/eigen
 cmake .. -DEIGEN3_INCLUDE_DIR=eigen -DPYTHON=`which python`
 make
 cd python
