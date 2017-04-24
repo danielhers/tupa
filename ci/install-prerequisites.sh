535719731858bb33414c5115f4b9d2a91acccfa5
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2017-04-24 17:09:39 +0300

    Avoid error message when directories already exist

--- ci/install-prerequisites.sh
+++ ci/install-prerequisites.sh
@@ -7,16 +7,28 @@ git submodule update --init --recursive
 pip install -r requirements.txt
 python -m spacy download en > spacy.log
 python -m nltk.downloader wordnet propbank
+rm -rf ~/nltk_data/corpora/propbank
 git clone https://github.com/propbank/propbank-frames ~/nltk_data/corpora/propbank
 
 # Install DyNet
-export CXX="g++-4.8" CC="gcc-4.8"
-git clone https://github.com/clab/dynet
-cd dynet
-hg clone https://bitbucket.org/eigen/eigen
-mkdir build
-ln -s eigen build/
+if [ -d dynet ]; then
+  cd dynet
+  git pull https://github.com/clab/dynet
+else
+  git clone https://github.com/clab/dynet
+  cd dynet
+fi
+if [ -d eigen ]; then
+  cd eigen
+  hg pull --update
+  cd ..
+else
+  hg clone https://bitbucket.org/eigen/eigen
+fi
+mkdir -p build
+[ -d build/eigen ] || ln -sf eigen build/
 cd build
+export CXX="g++-4.8" CC="gcc-4.8"
 cmake .. -DEIGEN3_INCLUDE_DIR=eigen -DPYTHON=`which python`
 make
 cd python
