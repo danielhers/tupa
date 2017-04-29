7662ad03a3cfff5e33b9ec21853c6aa9ec8948e9
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2017-04-29 21:31:33 +0300

    Fix #1: path to eigen in dynet installation

--- ci/install-dynet.sh
+++ ci/install-dynet.sh
@@ -6,7 +6,7 @@ cd dynet
 hg clone https://bitbucket.org/eigen/eigen
 mkdir build
 cd build
-cmake .. -DEIGEN3_INCLUDE_DIR=eigen -DPYTHON=`which python`
+cmake .. -DEIGEN3_INCLUDE_DIR=../eigen -DPYTHON=`which python`
 make
 cd python
 python setup.py install
