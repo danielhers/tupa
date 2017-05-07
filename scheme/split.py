6ab8e1fa8fda4bc829bf35db413c09466de67342
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-05-07 10:01:17 +0300

    Script to split AMRs to files

---
+++ scheme/split.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import argparse
+import sys
+
+from util.amr import *
+
+desc = """Split AMRs to separate files (important for shuffling before training the parser)"""
+
+
+def main(args):
+    lines = []
+    amr_id = None
+    with open(args.filename, encoding="utf-8") as f:
+        for line in f:
+            clean = line.lstrip()
+            if clean:
+                if clean[0] != COMMENT_PREFIX or re.match("#\s*::", clean):
+                    lines.append(line)
+                    m = re.match(ID_PATTERN, clean)
+                    if m:
+                        amr_id = m.group(1)
+            elif lines:
+                write_file(args.outdir, amr_id, lines)
+        if lines:
+            write_file(args.outdir, amr_id, lines)
+
+
+def write_file(outdir, amr_id, lines):
+    filename = outdir + os.sep + amr_id + ".txt"
+    with open(filename, "w", encoding="utf-8") as f:
+        f.writelines(lines)
+    lines.clear()
+    print("\rWrote %-70s" % filename, end="", flush=True)
+
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser(description=desc)
+    argparser.add_argument("filename", help="file name to split")
+    argparser.add_argument("outdir", help="output directory")
+    main(argparser.parse_args())
+    sys.exit(0)
