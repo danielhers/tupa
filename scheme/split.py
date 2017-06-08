04f7752994d7cde7df49c03768c3c07833778050
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-06-08 16:04:01 +0300

    Add --quiet option to split.py

--- scheme/split.py
+++ scheme/split.py
@@ -31,22 +31,24 @@ def main(args):
                         if m:
                             amr_id = m.group(1)
                 elif lines:
-                    write_file(args.outdir, amr_id, lines)
+                    write_file(args.outdir, amr_id, lines, quiet=args.quiet)
             if lines:
-                write_file(args.outdir, amr_id, lines)
+                write_file(args.outdir, amr_id, lines, quiet=args.quiet)
 
 
-def write_file(outdir, amr_id, lines):
+def write_file(outdir, amr_id, lines, quiet=False):
     filename = outdir + os.sep + amr_id + ".txt"
     with open(filename, "w", encoding="utf-8") as f:
         f.writelines(lines)
     lines.clear()
-    print("\rWrote %-70s" % filename, end="", flush=True)
+    if not quiet:
+        print("\rWrote %-70s" % filename, end="", flush=True)
 
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(description=desc)
     argparser.add_argument("filename", help="file name to split")
     argparser.add_argument("outdir", help="output directory")
+    argparser.add_argument("-q", "--quiet", action="store_true", help="less output")
     main(argparser.parse_args())
     sys.exit(0)
