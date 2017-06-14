fdc1434d34972a099fd5ea2fbd30b942361f10bb
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-06-14 12:56:44 +0300

    Install DyNet using pip
    Split passages on double newline when reading text, even if -s not given

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -9,38 +9,4 @@ python -m spacy download en > spacy.log
 python -m nltk.downloader propbank
 rm -rf ~/nltk_data/corpora/propbank
 git clone https://github.com/propbank/propbank-frames ~/nltk_data/corpora/propbank
-
-# Install DyNet
-if [ -d dynet/.git ]; then
-  cd dynet
-  git pull
-else
-  rm -rf dynet
-  git clone https://github.com/clab/dynet
-  cd dynet
-fi
-if [ -d eigen ]; then
-  cd eigen
-  hg pull --update
-  cd ..
-else
-  hg clone https://bitbucket.org/eigen/eigen
-fi
-mkdir -p build
-[ -d build/eigen ] || ln -sf ../eigen build/
-cd build
-if [ -z ${CXX+x} ]; then
-    if hash g++-4.9 2>/dev/null; then
-      export CXX="g++-4.9" CC="gcc-4.9"
-    elif hash g++-4.8 2>/dev/null; then
-      export CXX="g++-4.8" CC="gcc-4.8"
-    fi
-fi
-if [ -z ${BOOST+x} ]; then
-  cmake .. -DEIGEN3_INCLUDE_DIR=eigen -DPYTHON=`which python`
-else
-  cmake .. -DEIGEN3_INCLUDE_DIR=eigen -DBOOST_ROOT:PATHNAME=$BOOST -DBoost_NO_BOOST_CMAKE=TRUE -DBoost_NO_SYSTEM_PATHS=TRUE -DBoost_LIBRARY_DIRS:FILEPATH=$BOOST/lib -DPYTHON=`which python`
-fi
-make
-cd python
-python setup.py install
+pip install git+https://github.com/clab/dynet#egg=dynet
