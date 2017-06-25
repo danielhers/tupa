f281cfddc1e35e112704fb849ad8473a4a3869cf
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-06-25 17:39:48 +0300

    Download AMR resources as dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -10,3 +10,7 @@ python -m nltk.downloader propbank
 rm -rf ~/nltk_data/corpora/propbank
 git clone https://github.com/propbank/propbank-frames ~/nltk_data/corpora/propbank
 pip install git+https://github.com/clab/dynet#egg=dynet
+
+# Download AMR resources
+cd "$(dirname "$0")"/scheme/resources
+curl --remote-name-all https://amr.isi.edu/download/lists/{{have-{org,rel}-role-91-roles,verbalization-list}-v1.06,morph-verbalization-v1.01}.txt
