ab9fd80c9f0d17d10e1bc52f238d95b1920469c4
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-07-20 14:13:34 +0300

    Append categories to node labels based on AMR resources
    Helps provide information regarding the node semantics, even when the
    label is escaped with a placeholder representing the terminal yield.
    This way there is no blow up of node labels but there is still info.
    Using have-org-role-91-roles-v1.06.txt and
    have-rel-role-91-roles-v1.06.txt

--- scheme/util/amr.py
+++ scheme/util/amr.py
@@ -27,6 +27,7 @@ ALIGNMENT_SEP = ","
 PLACEHOLDER = re.compile("<[^>]*>")
 SKIP_TOKEN = re.compile("[<>@]+")
 LABEL_ATTRIB = "label"
+LABEL_SEPARATOR = "|"  # after the separator there is the label category
 KNOWN_LABELS = set()  # used to avoid escaping when unnecessary
 PUNCTUATION_REMOVER = str.maketrans("", "", string.punctuation)
 PREFIXED_RELATION_ENUM = ("op", "snt")
@@ -75,6 +76,7 @@ LAYERS = {
 NEGATIONS = {}
 VERBALIZATION = {}
 ROLESETS = {}
+CATEGORIES = {}
 
 
 def read_resources():
@@ -86,8 +88,15 @@ def read_resources():
             lines = (re.findall(r'::DERIV\S*-(\S)\S+ "(\S+)"', l) for l in f if l and l[0] != "#")
             VERBALIZATION.update({w: l for l in lines for (_, w) in l})
         with open("verbalization-list-v1.06.txt", encoding="utf-8") as f:
-            lines = (re.findall(r"VERBALIZE (\S)+ TO.* (\S+-\d+)", l)[0] for l in f if l and l[0] not in "#D")
-            VERBALIZATION.update({w: [("V", v)] for w, v in lines})
+            lines = (re.findall(r" (\S+) TO *(\S+ :\S+)? (\S+-\d+) *(\S+)?", l)[0] for l in f if l and l[0] not in "#D")
+            for word, category, verb, suffix in lines:
+                VERBALIZATION.setdefault(word, []).append(("V", verb))
+                if category or suffix:
+                    CATEGORIES[word] = category.replace(" ", "") + suffix
+        with open("have-org-role-91-roles-v1.06.txt", encoding="utf-8") as f:
+            CATEGORIES.update(l.split()[::-1] for l in f if l and l[0] not in "#")
+        with open("have-rel-role-91-roles-v1.06.txt", encoding="utf-8") as f:
+            CATEGORIES.update(re.findall(r"(\S+) (\S+(?: [^:#]\S)*)", l)[0][::-1] for l in f if l and l[0] not in "#")
         with open("rolesets.txt", encoding="utf-8") as f:
             ROLESETS.update({l[0]: tuple(l[1:]) for l in csv.reader(f)})
     finally:
@@ -155,11 +164,11 @@ def is_valid_arg(node, label, *tags, is_parent=True):
 
 def resolve_label(node, label=None, reverse=False):
     """
-    Replace any placeholder in the node's label with the corresponding terminals' text
+    Replace any placeholder in the node's label with the corresponding terminals' text, and remove label category suffix
     :param node: node whose label is to be resolved
     :param label: the label if not taken from the node directly
-    :param reverse: if True, *introduce* placeholders into the label rather than removing them
-    :return: the resolved label, with or without placeholders (depending on the value of reverse)
+    :param reverse: if True, *introduce* placeholders and categories into the label rather than removing them
+    :return: the resolved label, with or without placeholders and categories (depending on the value of reverse)
     """
     def _replace(old, new):  # replace only inside the label value/name
         new = new.strip('"()')
@@ -173,39 +182,49 @@ def resolve_label(node, label=None, reverse=False):
             label = node.label
         except AttributeError:
             label = node.attrib.get(LABEL_ATTRIB)
-    if label is not None and not (reverse and label in KNOWN_LABELS):
-        children = [c.children[0] if c.tag == "PNCT" else c for c in node.children]
-        terminals = sorted([c for c in children if getattr(c, "text", None)],
-                           key=lambda c: getattr(c, "index", getattr(c, "position", None)))
-        if terminals:
-            if not reverse and label.startswith(NUM + "("):  # numeric label (always 1 unless "numbers" layer is on)
-                number = terminals_to_number(terminals)  # try replacing spelled-out numbers/months with digits
-                if number is not None:
-                    label = "%s(%s)" % (NUM, number)
-            else:
-                if len(terminals) > 1:
-                    label = _replace("<t>", "".join(t.text for t in terminals))
-                for i, terminal in enumerate(terminals):
-                    label = _replace("<t%d>" % i, terminal.text)
-                    label = _replace("<T%d>" % i, terminal.text.title())
-                    try:
-                        lemma = terminal.lemma
-                    except AttributeError:
-                        lemma = terminal.extra.get(textutil.LEMMA_KEY)
-                    if lemma == "-PRON-":
-                        lemma = terminal.text.lower()
-                    lemma = lemma.translate(PUNCTUATION_REMOVER)
-                    label = _replace("<l%d>" % i, lemma)
-                    label = _replace("<L%d>" % i, lemma.title())
-                    negation = NEGATIONS.get(terminal.text)
-                    if negation is not None:
-                        label = _replace("<n%d>" % i, negation)
-                    morph = VERBALIZATION.get(terminal.text)
-                    if morph is not None:
-                        for prefix, value in morph:  # V: verb, N: noun, A: noun actor
-                            label = _replace("<%s%d>" % (prefix, i), value)
-    if reverse:
-        KNOWN_LABELS.add(label)
+    if label is not None:
+        category = None
+        if reverse:
+            category = CATEGORIES.get(label)  # category suffix to append to label
+        elif LABEL_SEPARATOR in label:
+            label = label[:label.find(LABEL_SEPARATOR)]  # remove category suffix
+        if not reverse or label not in KNOWN_LABELS:
+            children = [c.children[0] if c.tag == "PNCT" else c for c in node.children]
+            terminals = sorted([c for c in children if getattr(c, "text", None)],
+                               key=lambda c: getattr(c, "index", getattr(c, "position", None)))
+            if terminals:
+                if not reverse and label.startswith(NUM + "("):  # numeric label (always 1 unless "numbers" layer is on)
+                    number = terminals_to_number(terminals)  # try replacing spelled-out numbers/months with digits
+                    if number is not None:
+                        label = "%s(%s)" % (NUM, number)
+                else:
+                    if len(terminals) > 1:
+                        label = _replace("<t>", "".join(t.text for t in terminals))
+                    for i, terminal in enumerate(terminals):
+                        label = _replace("<t%d>" % i, terminal.text)
+                        label = _replace("<T%d>" % i, terminal.text.title())
+                        try:
+                            lemma = terminal.lemma
+                        except AttributeError:
+                            lemma = terminal.extra.get(textutil.LEMMA_KEY)
+                        if lemma == "-PRON-":
+                            lemma = terminal.text.lower()
+                        lemma = lemma.translate(PUNCTUATION_REMOVER)
+                        if reverse and category is None:
+                            category = CATEGORIES.get(lemma)
+                        label = _replace("<l%d>" % i, lemma)
+                        label = _replace("<L%d>" % i, lemma.title())
+                        negation = NEGATIONS.get(terminal.text)
+                        if negation is not None:
+                            label = _replace("<n%d>" % i, negation)
+                        morph = VERBALIZATION.get(terminal.text)
+                        if morph is not None:
+                            for prefix, value in morph:  # V: verb, N: noun, A: noun actor
+                                label = _replace("<%s%d>" % (prefix, i), value)
+        if reverse:
+            KNOWN_LABELS.add(label)
+            if category is not None:
+                label += LABEL_SEPARATOR + category
     return label
 
 
