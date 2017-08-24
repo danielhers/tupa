a3bb9ec3f6f33c19098f4ab7fce72fa1d6bf15cd
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-08-24 10:42:33 +0300

    Fix unlabeled SDP evaluation to really be unlabeled

--- scheme/evaluation/sdp.py
+++ scheme/evaluation/sdp.py
@@ -8,13 +8,14 @@ EVAL_TYPES = (evaluation.LABELED, evaluation.UNLABELED)
 
 def get_scores(s1, s2, eval_type, verbose):
     converter = SdpConverter()
-    edges = [{e for nodes, _ in converter.build_nodes(s) for n in nodes for e in n.outgoing} for s in (s1, s2)]
+    edges = [[e for nodes, _ in converter.build_nodes(s) for n in nodes for e in n.outgoing] for s in (s1, s2)]
     if eval_type == evaluation.UNLABELED:
         for es in edges:
             for e in es:
                 e.rel = None
-    res = evaluation.EvaluatorResults({PRIMARY: evaluation.SummaryStatistics(
-        len(edges[0] & edges[1]), len(edges[0] - edges[1]), len(edges[1] - edges[0]))}, default={PRIMARY.name: PRIMARY})
+    g, r = map(set, edges)
+    res = evaluation.EvaluatorResults({PRIMARY: evaluation.SummaryStatistics(len(g & r), len(g - r), len(r - g))},
+                                      default={PRIMARY.name: PRIMARY})
     if verbose:
         print("Evaluation type: (" + eval_type + ")")
         res.print()
