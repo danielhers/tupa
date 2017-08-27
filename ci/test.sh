50e757ae1ed07f4df77373c1c68b508b36e5e380
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-08-27 15:16:57 +0300

    Rename toy AMR passage to .amr suffix; separate toy tests from unit

--- ci/test.sh
+++ ci/test.sh
@@ -6,23 +6,29 @@ FORMAT=${TEST_SUITE#*-}
 # download data
 case "$FORMAT" in
 ucca)
-    mkdir pickle
-    curl -L http://www.cs.huji.ac.il/~danielh/ucca/ucca_corpus_pickle.tgz | tar xz -C pickle || curl -L https://www.dropbox.com/s/q4ycn45zlmhuf9k/ucca_corpus_pickle.tgz | tar xz -C pickle
-    python -m scripts.split_corpus -q pickle -t 4282 -d 454 -l
+    if [[ "$ACTION" != toy ]]; then
+        mkdir pickle
+        curl -L http://www.cs.huji.ac.il/~danielh/ucca/ucca_corpus_pickle.tgz | tar xz -C pickle || curl -L https://www.dropbox.com/s/q4ycn45zlmhuf9k/ucca_corpus_pickle.tgz | tar xz -C pickle
+        python -m scripts.split_corpus -q pickle -t 4282 -d 454 -l
+    fi
     TOY_DATA=test_files/504.xml
     export PARAMS_NUM=5
     ;;
 amr)
-    curl --remote-name-all https://amr.isi.edu/download/2016-03-14/alignment-release-{training,dev,test}-bio.txt
-    python scheme/split.py -q alignment-release-training-bio.txt alignment-release-training-bio
-    TOY_DATA=test_files/LDC2014T12.txt
+    if [[ "$ACTION" != toy ]]; then
+        curl --remote-name-all https://amr.isi.edu/download/2016-03-14/alignment-release-{training,dev,test}-bio.txt
+        python scheme/split.py -q alignment-release-training-bio.txt alignment-release-training-bio
+    fi
+    TOY_DATA=test_files/LDC2014T12.amr
     CONVERT_DATA=alignment-release-dev-bio.txt
     ;;
 sdp)
-    mkdir data
-    curl -L http://svn.delph-in.net/sdp/public/2015/trial/current.tgz | tar xz -C data
-    python scheme/split.py -q data/sdp/trial/dm.sdp data/sdp/trial/dm
-    python -m scripts.split_corpus -q data/sdp/trial/dm -t 120 -d 36 -l
+    if [[ "$ACTION" != toy ]]; then
+        mkdir data
+        curl -L http://svn.delph-in.net/sdp/public/2015/trial/current.tgz | tar xz -C data
+        python scheme/split.py -q data/sdp/trial/dm.sdp data/sdp/trial/dm
+        python -m scripts.split_corpus -q data/sdp/trial/dm -t 120 -d 36 -l
+    fi
     TOY_DATA=test_files/20001001.sdp
     CONVERT_DATA=data/sdp/trial/*.sdp
     export PARAMS_NUM=3
@@ -33,15 +39,13 @@ case "$TEST_SUITE" in
 unit)
     # unit tests
     python -m unittest discover -v || exit 1
+    ;;
+toy-*)
     # basic parser tests
     for m in "" --sentences --paragraphs; do
-      python tupa/parse.py -I 10 -t test_files/504.xml -d test_files/504.xml $m -m "model_toy$m" -v || exit 1
-      python tupa/parse.py test_files/504.xml $m -em "model_toy$m" -v || exit 1
+      python tupa/parse.py -I 10 -t "$TOY_DATA" -d "$TOY_DATA" $m -m "model_$FORMAT$m" -v || exit 1
+      python tupa/parse.py "$TOY_DATA" $m -em "model_$FORMAT$m" -v || exit 1
     done
-    python tupa/parse.py -f amr -I 10 -t test_files/LDC2014T12.txt -d test_files/LDC2014T12.txt -m model_LDC2014T12 -v || exit 1
-    python tupa/parse.py -f amr test_files/LDC2014T12.txt -em model_LDC2014T12 -v || exit 1
-    python tupa/parse.py -I 10 -t test_files/20001001.sdp -d test_files/20001001.sdp -m model_dm -v || exit 1
-    python tupa/parse.py test_files/20001001.sdp -em model_dm -v || exit 1
     ;;
 sparse-ucca|mlp-ucca|bilstm-ucca|noop-ucca)
     python tupa/parse.py -v -c "$ACTION" --max-words-external=5000 --layer-dim=100 -We pickle/dev/*0.pickle -t pickle/train/*0.pickle
