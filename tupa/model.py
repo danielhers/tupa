ef7f3d8ccddc40d2e88c0c8c0271d80981739bbd
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-08-28 10:05:59 +0300

    Avoid repetition in parameter attribute definition

--- tupa/model.py
+++ tupa/model.py
@@ -9,74 +9,91 @@ ACTION_AXIS = 0
 LABEL_AXIS = 1
 
 
+class ParamDef(object):
+    def __init__(self, name, **param_attr_to_config_attr):
+        self.name = name
+        self.param_attr_to_config_attr = param_attr_to_config_attr
+
+    def create_from_config(self):
+        args = Config().args
+        return FeatureParameters(self.name, **{k: getattr(args, v) if hasattr(args, v) else v
+                                               for k, v in self.param_attr_to_config_attr.items()})
+
+    def load_to_config(self, params):
+        param = params.get(self.name)
+        if param is not None:
+            args = Config().args
+            Config().update({v: getattr(param, k) for k, v in self.param_attr_to_config_attr.items()
+                             if hasattr(args, v)})
+
+
+PARAM_DEFS = (
+    ParamDef("n", dim="node_label_dim",    size="max_node_labels", min_count="min_node_label_count"),
+    ParamDef("c", dim="node_category_dim", size="max_node_categories"),
+    ParamDef("W", dim="word_dim_external", size="max_words_external", dropout="word_dropout_external",
+             updated="update_word_vectors", filename="word_vectors", copy_from="w"),
+    ParamDef("w", dim="word_dim",          size="max_words",          dropout="word_dropout"),
+    ParamDef("t", dim="tag_dim",           size="max_tags"),
+    ParamDef("d", dim="dep_dim",           size="max_deps"),
+    ParamDef("e", dim="edge_label_dim",    size="max_edge_labels"),
+    ParamDef("p", dim="punct_dim",         size="max_puncts"),
+    ParamDef("A", dim="action_dim",        size="max_action_types"),
+    ParamDef("T", dim="ner_dim",           size="max_ner_types"),
+)
+
+
 class Model(object):
     def __init__(self, model_type, filename, feature_extractor=None, model=None):
-        if model_type is None:
-            model_type = SPARSE
-        self.model_type = model_type
+        self.args = Config().args
+        self.model_type = model_type or SPARSE
         self.filename = filename
         self.actions = Actions()
         self.labels = None
-        if feature_extractor is not None or model is not None:
+        if feature_extractor or model:
             self.feature_extractor = feature_extractor
             self.model = model
             self.model.input_params = self.feature_extractor.params
             self.load_labels()
             return
 
-        max_values = [Config().args.max_action_labels]
+        max_values = [self.args.max_action_labels]
         values = [self.actions.all]
-        node_labels = FeatureParameters("n", Config().args.node_label_dim, Config().args.max_node_labels,
-                                        min_count=Config().args.min_node_label_count)
-        self.feature_params = [
-            node_labels,
-            FeatureParameters("c", Config().args.node_category_dim, Config().args.max_node_categories),
-            FeatureParameters("W", Config().args.word_dim_external, Config().args.max_words_external,
-                              Config().args.word_dropout_external, Config().args.update_word_vectors, copy_from="w",
-                              filename=Config().args.word_vectors),
-            FeatureParameters("w", Config().args.word_dim, Config().args.max_words, Config().args.word_dropout),
-            FeatureParameters("t", Config().args.tag_dim, Config().args.max_tags),
-            FeatureParameters("d", Config().args.dep_dim, Config().args.max_deps),
-            FeatureParameters("e", Config().args.edge_label_dim, Config().args.max_edge_labels),
-            FeatureParameters("p", Config().args.punct_dim, Config().args.max_puncts),
-            FeatureParameters("A", Config().args.action_dim, Config().args.max_action_types),
-            FeatureParameters("T", Config().args.ner_dim, Config().args.max_ner_types),
-        ]
+        self.feature_params = [p.create_from_config() for p in PARAM_DEFS]
+        node_labels = self.feature_params[0]
         FeatureEnumerator.init_data(node_labels)
         if Config().node_labels and node_labels.size:
             self.labels = node_labels.data
             values.append(self.labels.all)
-            max_values.append(Config().args.max_node_labels)
+            max_values.append(self.args.max_node_labels)
 
-        if model_type == SPARSE:
+        if self.model_type == SPARSE:
             from .features.sparse_features import SparseFeatureExtractor
             from .classifiers.linear.sparse_perceptron import SparsePerceptron
             self.feature_extractor = SparseFeatureExtractor()
             self.model = SparsePerceptron(filename, values)
-        elif model_type == MLP_NN:
+        elif self.model_type == MLP_NN:
             from .classifiers.nn.feedforward import MLP
             from .features.dense_features import DenseFeatureExtractor
             self.feature_extractor = FeatureEnumerator(DenseFeatureExtractor(), self.feature_params)
             self.model = MLP(filename, values, self.feature_extractor.params, max_num_labels=max_values)
-        elif model_type == BILSTM_NN:
+        elif self.model_type == BILSTM_NN:
             from .classifiers.nn.bilstm import BiLSTM
             from .features.dense_features import DenseFeatureExtractor
             self.feature_extractor = FeatureIndexer(FeatureEnumerator(DenseFeatureExtractor(), self.feature_params))
             self.model = BiLSTM(filename, values, self.feature_extractor.params, max_num_labels=max_values)
-        elif model_type == NOOP:
+        elif self.model_type == NOOP:
             from .features.empty_features import EmptyFeatureExtractor
             from .classifiers.noop import NoOp
             self.feature_extractor = EmptyFeatureExtractor()
             self.model = NoOp(filename, values)
         else:
-            raise ValueError("Invalid model type: '%s'" % model_type)
+            raise ValueError("Invalid model type: '%s'" % self.model_type)
 
     def init_features(self, state, train):
         self.model.init_features(self.feature_extractor.init_features(state), train)
 
     def finalize(self, finished_epoch):
-        return Model(model_type=self.model_type,
-                     filename=self.filename,
+        return Model(model_type=self.model_type, filename=self.filename,
                      feature_extractor=self.feature_extractor.finalize(),
                      model=self.model.finalize(finished_epoch=finished_epoch))
 
@@ -102,55 +119,12 @@ class Model(object):
                 raise IOError("Failed loading model from '%s'" % self.filename) from e
 
     def load_params_to_config(self):
-        p = self.feature_extractor.params.get("n")
-        if p is not None:
-            Config().args.node_label_dim = p.dim
-            Config().args.max_node_labels = p.size
-            Config().args.min_node_label_count = p.min_count
-        p = self.feature_extractor.params.get("c")
-        if p is not None:
-            Config().args.node_category_dim = p.dim
-            Config().args.max_node_categories = p.size
-        p = self.feature_extractor.params.get("W")
-        if p is not None:
-            Config().args.word_dim_external = p.dim
-            Config().args.max_words_external = p.size
-            Config().args.word_dropout_external = p.dropout
-            Config().args.update_word_vectors = p.updated
-            Config().args.word_vectors = p.filename
-        p = self.feature_extractor.params.get("w")
-        if p is not None:
-            Config().args.word_dim = p.dim
-            Config().args.max_words = p.size
-            Config().args.word_dropout = p.dropout
-        p = self.feature_extractor.params.get("t")
-        if p is not None:
-            Config().args.tag_dim = p.dim
-            Config().args.max_tags = p.size
-        p = self.feature_extractor.params.get("d")
-        if p is not None:
-            Config().args.dep_dim = p.dim
-            Config().args.max_deps = p.size
-        p = self.feature_extractor.params.get("e")
-        if p is not None:
-            Config().args.edge_label_dim = p.dim
-            Config().args.max_edge_labels = p.size
-        p = self.feature_extractor.params.get("p")
-        if p is not None:
-            Config().args.punct_dim = p.dim
-            Config().args.max_puncts = p.size
-        p = self.feature_extractor.params.get("A")
-        if p is not None:
-            Config().args.action_dim = p.dim
-            Config().args.max_action_types = p.size
-        p = self.feature_extractor.params.get("T")
-        if p is not None:
-            Config().args.ner_dim = p.dim
-            Config().args.max_ner_types = p.size
+        for param in PARAM_DEFS:
+            param.load_to_config(self.feature_extractor.params)
         if hasattr(self.model, "max_num_labels"):
-            Config().args.max_action_labels = self.model.max_num_labels[ACTION_AXIS]
+            self.args.max_action_labels = self.model.max_num_labels[ACTION_AXIS]
             if len(self.model.labels) > 1:
-                Config().args.max_node_labels = self.model.max_num_labels[LABEL_AXIS]
+                self.args.max_node_labels = self.model.max_num_labels[LABEL_AXIS]
 
     def load_labels(self):
         self.actions.all = self.model.labels[ACTION_AXIS]
