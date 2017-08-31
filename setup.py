922a546f38ab6542034a7dc9b84053102ae6fc93
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-08-31 13:21:08 +0300

    Increment version to 1.2.0 (incompatible with previously trained models)

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ class install(_install):
 
 
 setup(name="TUPA",
-      version="1.1.4",
+      version="1.2.0",
       description="Transition-based UCCA Parser",
       long_description=long_description,
       classifiers=[
