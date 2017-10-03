a04de59d612c7446e738a1f020bc704f18fedcbb
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-10-03 12:24:16 +0300

    Fix bug where softmax layer is omitted

--- tupa/classifiers/nn/mlp.py
+++ tupa/classifiers/nn/mlp.py
@@ -11,6 +11,7 @@ class MultilayerPerceptron(object):
         self.model = model
         self.params = global_params
         self.layers = self.args.layers if layers is None else layers
+        self.total_layers = self.layers
         self.layer_dim = self.args.layer_dim if layer_dim is None else layer_dim
         self.output_dim = self.args.output_dim if output_dim is None else output_dim
         self.init = CategoricalParameter(INITIALIZERS, self.args.init)
@@ -26,20 +27,21 @@ class MultilayerPerceptron(object):
         if self.num_labels:  # Adding another layer at the top
             i_dim.append(self.output_dim)
             o_dim.append(self.num_labels)
+            self.total_layers += 1
         self.params.update((key(prefix, i, **self.key_args), self.model.add_parameters(dims[i], init=self.init()))
                            for prefix, dims in (("W", list(zip(o_dim, i_dim))), ("b", o_dim))
                            for i, dim in enumerate(dims))
 
     def evaluate(self, inputs, train=False):
         x = dy.concatenate(list(inputs))
-        for i in range(self.layers):
+        for i in range(self.total_layers):
             try:
                 if train and self.dropout:
                     x = dy.dropout(x, self.dropout)
                 W, b = [dy.parameter(self.params[key(prefix, i, **self.key_args)]) for prefix in ("W", "b")]
                 x = self.activation()(W * x + b)
             except ValueError as e:
-                raise ValueError("Error in evaluating layer %d of %d" % (i + 1, self.layers)) from e
+                raise ValueError("Error in evaluating layer %d of %d" % (i + 1, self.total_layers)) from e
         return x
 
 
