cf1da0b19e2aa92b7550441ba95ec6600904f900
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2017-10-20 21:44:03 +0300

    Update version to 1.2.2

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ class install(_install):
 
 
 setup(name="TUPA",
-      version="1.2.1",
+      version="1.2.2",
       description="Transition-based UCCA Parser",
       long_description=long_description,
       classifiers=[
