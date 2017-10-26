cc2ef4c8515d9adda0b62e889196feb58c5850d0
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-10-26 11:27:31 +0300

    Check README reST formatting before deploy

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -xe
 
-pip install pypandoc twine
+pip install collective.checkdocs pypandoc twine
+python setup.py checkdocs || exit 1
 python setup.py sdist bdist_wheel
 twine upload --skip-existing dist/*
