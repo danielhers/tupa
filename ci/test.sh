6aeae33e74797b90099b0d801958886d3a90796f
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-11-09 11:43:41 +0200

    Smaller model in test

--- ci/test.sh
+++ ci/test.sh
@@ -15,12 +15,16 @@ if ! [[ "$ACTION" =~ ^(toy|unit)$ ]]; then
         mkdir pickle
         curl -L http://www.cs.huji.ac.il/~danielh/ucca/ucca_corpus_pickle.tgz | tar xz -C pickle || curl -L https://www.dropbox.com/s/q4ycn45zlmhuf9k/ucca_corpus_pickle.tgz | tar xz -C pickle
         python -m scripts.split_corpus -q pickle -t 4282 -d 454 -l
+        TRAIN_DATA="pickle/train/*0.pickle"
+        DEV_DATA="pickle/dev/*0.pickle"
         ;;
     amr)
         curl --remote-name-all https://amr.isi.edu/download/2016-03-14/alignment-release-{training,dev,test}-bio.txt
         rename 's/.txt/.amr/' alignment-release-*-bio.txt
         python scheme/split.py -q alignment-release-training-bio.amr alignment-release-training-bio
         CONVERT_DATA=alignment-release-dev-bio.amr
+        TRAIN_DATA=alignment-release-training-bio
+        DEV_DATA=alignment-release-dev-bio.amr
         ;;
     sdp)
         mkdir data
@@ -28,6 +32,8 @@ if ! [[ "$ACTION" =~ ^(toy|unit)$ ]]; then
         python scheme/split.py -q data/sdp/trial/dm.sdp data/sdp/trial/dm
         python -m scripts.split_corpus -q data/sdp/trial/dm -t 120 -d 36 -l
         CONVERT_DATA=data/sdp/trial/*.sdp
+        TRAIN_DATA=data/sdp/trial/dm/train
+        DEV_DATA=data/sdp/trial/dm/dev
         ;;
     esac
 fi
@@ -43,9 +49,6 @@ toy-*)  # basic parser tests
       python tupa/parse.py "$TOY_DATA" $m -em "model_$FORMAT$m" -v || exit 1
     done
     ;;
-sparse-ucca|mlp-ucca|bilstm-ucca|noop-ucca)
-    python tupa/parse.py -vv -c "$ACTION" --max-words-external=5000 --layer-dim=100 -We "pickle/dev/*0.pickle" -t "pickle/train/*0.pickle"
-    ;;
 tune-*)
     export PARAMS_NUM=3
     while :; do
@@ -53,16 +56,16 @@ tune-*)
     done
     column -t -s, params.csv
     ;;
-sparse-amr)
-    python tupa/parse.py -vv -c sparse --max-node-labels=250 -We "$TOY_DATA" -t "alignment-release-training-bio/*10.amr"
-    ;;
-noop-amr)
-    python tupa/parse.py -vv -c noop -We -t alignment-release-training-bio alignment-release-dev-bio.amr
-    ;;
-sparse-sdp|mlp-sdp|bilstm-sdp|noop-sdp)
-    python tupa/parse.py -vv -c "$ACTION" -We data/sdp/trial/dm/dev -t data/sdp/trial/dm/train
-    ;;
 convert-*)
     python scheme/convert_and_evaluate.py "$CONVERT_DATA" -v
+    ;;
+noop-amr)
+    python tupa/parse.py -vv -c noop -We -t "$TRAIN_DATA" "$DEV_DATA"
+    ;;
+*-amr)
+    python tupa/parse.py -vv -c "$ACTION" -We "$TOY_DATA" -t "alignment-release-training-bio/*10.amr" --max-node-labels=250
+    ;;
+*)
+    python tupa/parse.py -vv -c "$ACTION" -We "$DEV_DATA" -t "$TRAIN_DATA" --max-words-external=5000 --word-dim=100 --lstm-layer-dim=100 --embedding-layer-dim=100
     ;;
 esac
