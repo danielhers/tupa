7857dfa83afba730dabb7e2d92396fb624763db6
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-11-15 15:13:46 +0200

    Fix export

--- tupa/classifiers/nn/neural_network.py
+++ tupa/classifiers/nn/neural_network.py
@@ -285,9 +285,9 @@ class NeuralNetwork(Classifier, SubModel):
 
     def get_all_params(self):
         d = super().get_all_params()
-        d.update(("input_" + s, p.data.all) for s, p in self.input_params.items())
+        d.update(("input_" + s, p.data.all) for s, p in self.input_params.items() if p.data)
         for model in self.sub_models():
-            for key, value in model.params:
+            for key, value in model.params.items():
                 for name, param in [("%s%s%d%d%d" % (key, p, i, j, k), v) for i, (f, b) in
                                     enumerate(value.builder_layers)
                                     for p, r in (("f", f), ("b", b)) for j, l in enumerate(r.get_parameters())
