78468389161af42739ee1e6f8618f8195c627dd1
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2017-11-21 15:40:27 +0200

    Fix one-primary-parent constraint not being checked in unlabeled parsing

--- scheme/constraint/amr.py
+++ scheme/constraint/amr.py
@@ -5,7 +5,7 @@ from ..util.amr import *
 class AmrConstraints(Constraints):
     def __init__(self, args):
         super().__init__(args, multigraph=True, require_implicit_childless=False, allow_root_terminal_children=True,
-                         possible_multiple_incoming=TERMINAL_DEP, childless_incoming_trigger=WIKI,
+                         possible_multiple_incoming={TERMINAL_DEP}, childless_incoming_trigger=WIKI,
                          childless_outgoing_allowed=TERMINAL_TAGS)
 
     def allow_action(self, action, history):
