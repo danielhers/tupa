c5c4a34558fd8d99035d31b7010acb875410e359
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-12-04 11:46:52 +0200

    Remove root constraint from CoNLL-U

--- scheme/constraint/conllu.py
+++ scheme/constraint/conllu.py
@@ -1,8 +1,6 @@
 from ..constraints import Constraints
 
-TOP_LEVEL = ("root",)
-
 
 class ConlluConstraints(Constraints):
     def __init__(self, args):
-        super().__init__(args, top_level_allowed=TOP_LEVEL, top_level_only=TOP_LEVEL)
+        super().__init__(args)
