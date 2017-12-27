1fd4acb4dcf4ba9f8ec3bbb06721afed0fe5a732
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-12-27 12:08:53 +0200

    Visualize loaded TUPA model rather than reimplementing DyNet IO

--- tupa/viz.py
+++ tupa/viz.py
@@ -3,59 +3,56 @@ import numpy as np
 
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
-import re
-import os
-from argparse import ArgumentParser
+from configargparse import ArgParser
 from scipy.misc import imresize
 from matplotlib.ticker import MaxNLocator
+from tupa.export import load_model
+from tqdm import tqdm
 
 np.seterr("raise")
 
+REPLACEMENTS = (
+    ("axes_", ""),
+    ("_", " "),
+)
 
-def smooth(x, s=100):
+
+def smooth(x, s):
     if len(x.shape) == 1:
         x = x.reshape((-1, x.shape[0]))
     return imresize(x, size=(min(x.shape[0], s), min(x.shape[1], s)))
 
 
-def main(args):
-    for filename in args.data:
-        basename, _ = os.path.splitext(filename)
-        print(filename)
-        values = {}
-        name = None
-        with open(filename) as f:
-            for line in f:
-                if line.startswith("#"):
-                    name = line
-                    print(name.strip())
-                else:
-                    _, key, shape, *_ = name.split()
-                    key = tuple(re.findall(r"(?<=/)[a-z]+(?=/)|(?<=_)\d+", key))
-                    if len(key) == 1:
-                        key = ("",) + key
-                    key = (key[0],) + tuple(map(int, key[1:]))
-                    shape = tuple(map(int, re.findall(r"\d+", shape)))
-                    values[key] = smooth(np.reshape(np.fromstring(line, sep=" "), shape), s=args.smoothing)
-        _, axes = plt.subplots(len(values), figsize=(19, 2 * len(values)))
-        plt.tight_layout()
-        for i, (n, v) in enumerate(sorted(values.items())):
-            plt.sca(axes[i] if len(values) > 1 else axes)
-            if len(v.shape) == 1:
-                plt.bar(range(len(v)), v)
-            else:
-                plt.colorbar(plt.pcolormesh(v))
-                plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True))
-            plt.ylabel(" ".join(map(str, n)))
-            plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
-        output_file = basename + ".png"
-        plt.savefig(output_file)
-        plt.clf()
-        print("Saved '%s'." % output_file)
+def visualize(model, filename, smoothing):
+    values = model.get_all_params()
+    for key, value in dict(values).items():
+        if not isinstance(value, np.ndarray):
+            del values[key]
+    _, axes = plt.subplots(len(values), figsize=(19, 2 * len(values)))
+    plt.tight_layout()
+    for i, (key, value) in enumerate(tqdm(values.items(), unit="param", desc=filename)):
+        plt.sca(axes[i] if len(values) > 1 else axes)
+        plt.colorbar(plt.pcolormesh(smooth(value, smoothing)))
+        for find, replace in REPLACEMENTS:
+            key = key.replace(find, replace)
+        plt.ylabel(key)
+        for axis in (plt.gca().xaxis, plt.gca().yaxis):
+            axis.set_major_locator(MaxNLocator(integer=True))
+    output_file = filename + ".png"
+    plt.savefig(output_file)
+    plt.clf()
+    print("Saved '%s'." % output_file)
+
+
+def main():
+    argparser = ArgParser(description="Load TUPA model and visualize, saving to .png file.")
+    argparser.add_argument("models", nargs="+", help="model file basename(s) to load")
+    argparser.add_argument("-s", "--smoothing", type=int, default=100)
+    args = argparser.parse_args()
+    for filename in args.models:
+        model = load_model(filename)
+        visualize(model, filename, args.smoothing)
 
 
 if __name__ == "__main__":
-    argparser = ArgumentParser()
-    argparser.add_argument("data", nargs="+")
-    argparser.add_argument("-s", "--smoothing", type=int, default=100)
-    main(argparser.parse_args())
+    main()
