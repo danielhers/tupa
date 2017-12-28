04836e97cf8a5ec7e0d2713c985e0912c39f9274
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-12-28 17:46:03 +0200

    Allow specifying single spec file for annotating multiple directories

--- scheme/annotate.py
+++ scheme/annotate.py
@@ -14,22 +14,26 @@ desc = """Read passages in any format, and write back with spaCy annotations."""
 
 
 def main(args):
-    if not os.path.isdir(args.out_dir):
-        os.mkdir(args.out_dir)
-    for pattern in args.filenames:
+    specs = [(pattern, args.out_dir, args.lang) for pattern in args.filenames]
+    if args.list_file:
+        with open(args.list_file, encoding="utf-8") as f:
+            specs += [l.strip().split() for l in f if not l.startswith("#")]
+    for pattern, out_dir, lang in specs:
+        os.makedirs(out_dir, exist_ok=True)
         filenames = glob(pattern)
         if not filenames:
             raise IOError("Not found: " + pattern)
         passages = read_files_and_dirs(filenames, converters=FROM_FORMAT)
         for passage in annotate_all(passages if args.verbose else
                                     tqdm(passages, unit=" passages", desc="Annotating " + os.path.basename(pattern)),
-                                    verbose=args.verbose, replace=True, lang=args.lang):
-            write_passage(passage, outdir=args.out_dir, verbose=args.verbose, binary=args.binary)
+                                    verbose=args.verbose, replace=True, lang=lang):
+            write_passage(passage, outdir=out_dir, verbose=args.verbose, binary=args.binary)
 
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(description=desc)
-    argparser.add_argument("filenames", nargs="+", help="passage file names to annotate")
+    argparser.add_argument("filenames", nargs="*", help="passage file names to annotate")
+    argparser.add_argument("-f", "--list-file", help="file whose rows are <PATTERN> <OUT-DIR> <LANGUAGE>")
     argparser.add_argument("-o", "--out-dir", default=".", help="directory to write annotated files to")
     argparser.add_argument("-b", "--binary", action="store_true", help="write in binary format (.%s)" % UCCA_EXT[1])
     argparser.add_argument("-l", "--lang", help="small two-letter language code to use for NLP model")
