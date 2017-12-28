1277790602507564d22f1f48f89c25fcb27b17a2
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-12-28 17:18:50 +0200

    Add --binary flag to annotate script

--- scheme/annotate.py
+++ scheme/annotate.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 from ucca.ioutil import read_files_and_dirs, write_passage
 from ucca.textutil import annotate_all
 
-from scheme.convert import FROM_FORMAT
+from scheme.convert import FROM_FORMAT, UCCA_EXT
 
 desc = """Read passages in any format, and write back with spaCy annotations."""
 
@@ -24,12 +24,13 @@ def main(args):
         for passage in annotate_all(passages if args.verbose else
                                     tqdm(passages, unit=" passages", desc="Annotating " + pattern),
                                     verbose=args.verbose, replace=True):
-            write_passage(passage, outdir=args.out_dir, verbose=args.verbose)
+            write_passage(passage, outdir=args.out_dir, verbose=args.verbose, binary=args.binary)
 
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(description=desc)
     argparser.add_argument("filenames", nargs="+", help="passage file names to annotate")
     argparser.add_argument("-o", "--out-dir", default=".", help="directory to write annotated files to")
+    argparser.add_argument("-b", "--binary", action="store_true", help="write in binary format (.%s)" % UCCA_EXT[1])
     argparser.add_argument("-v", "--verbose", action="store_true", help="print tagged text for each passage")
     main(argparser.parse_args())
