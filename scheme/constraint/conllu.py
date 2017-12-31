26c1f4cd586e79cf289b6aee217f30dd197cb2b2
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-12-31 14:03:02 +0200

    Force unique head in bilexical dependency parsing

--- scheme/constraint/conllu.py
+++ scheme/constraint/conllu.py
@@ -1,6 +1,7 @@
 from ..constraints import Constraints
+from ..conversion.conllu import ConlluConverter
 
 
 class ConlluConstraints(Constraints):
     def __init__(self, args):
-        super().__init__(args)
+        super().__init__(args, unique_outgoing={ConlluConverter.HEAD})
--- scheme/constraint/sdp.py
+++ scheme/constraint/sdp.py
@@ -6,4 +6,5 @@ TOP_LEVEL = (SdpConverter.ROOT, SdpConverter.TOP)
 
 class SdpConstraints(Constraints):
     def __init__(self, args):
-        super().__init__(args, top_level_allowed=TOP_LEVEL, top_level_only=TOP_LEVEL)
+        super().__init__(args, top_level_allowed=TOP_LEVEL, top_level_only=TOP_LEVEL,
+                         unique_outgoing={SdpConverter.HEAD})
