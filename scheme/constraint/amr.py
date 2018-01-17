ad19ea58662d9826a0dc5d550bad6d7e0ca9c0b5
Daniel Hershcovich <danielh@cs.huji.ac.il>
2018-01-17 14:01:00 +0200

    Fix parent and child AMR constraints to allow unlabeled edges

--- scheme/constraint/amr.py
+++ scheme/constraint/amr.py
@@ -16,12 +16,12 @@ class AmrConstraints(Constraints):
         return edge.tag in PREFIXED_RELATION_ENUM or edge not in edge.parent.outgoing
 
     def allow_parent(self, node, tag):
-        return (not node.implicit or tag not in TERMINAL_TAGS) and \
-               (node.label is None or (is_concept(node.label) or tag in TERMINAL_TAGS)) and \
-               is_valid_arg(node, node.label, tag)
+        return not tag or ((not node.implicit or tag not in TERMINAL_TAGS) and
+                           (node.label is None or (is_concept(node.label) or tag in TERMINAL_TAGS)) and
+                           is_valid_arg(node, node.label, tag))
 
     def allow_child(self, node, tag):
-        return is_valid_arg(node, node.label, tag, is_parent=False)
+        return not tag or is_valid_arg(node, node.label, tag, is_parent=False)
 
     def allow_label(self, node, label):
         return (is_concept(label) or node.outgoing_tags <= TERMINAL_TAGS and not node.is_root) and \
