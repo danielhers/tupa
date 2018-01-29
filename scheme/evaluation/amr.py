a460806af246ca4930fce097b5f2c1d425396773
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-01-29 15:17:26 +0200

    Unlabeled Smatch evaluation for AMR

--- scheme/evaluation/amr.py
+++ scheme/evaluation/amr.py
@@ -1,10 +1,32 @@
 import sys
 
+from ucca import evaluation
+from ucca.constructions import PRIMARY
+
 from ..util.amr import *
 
 sys.path.insert(0, os.path.dirname(importlib.util.find_spec("smatch.smatch").origin))  # to find amr.py from smatch
 from smatch import smatch
 sys.path.pop(0)
+
+EVAL_TYPES = (evaluation.LABELED, evaluation.UNLABELED)
+
+def get_scores(a1, a2, amr_id, eval_type, verbose):
+    if eval_type == evaluation.UNLABELED:
+        a1, a2 = [re.sub(":[a-zA-Z0-9-]*", ":label", a) for a in (a1, a2)]
+    try:
+        counts = smatch.process_amr_pair((a1, a2, amr_id))
+    except (AttributeError, IndexError):  # error in one of the AMRs
+        try:
+            counts = smatch.process_amr_pair((a2, a2, amr_id))
+            counts = (0, 0, counts[-1])  # best_match_num, test_triple_num
+        except (AttributeError, IndexError):  # error in ref AMR
+            counts = (0, 0, 1)  # best_match_num, test_triple_num, gold_triple_num
+    res = SmatchResults(*counts)
+    if verbose:
+        print("Evaluation type: (" + eval_type + ")")
+        res.print()
+    return res
 
 
 def evaluate(guessed, ref, converter=None, verbose=False, amr_id=None, **kwargs):
@@ -15,32 +37,29 @@ def evaluate(guessed, ref, converter=None, verbose=False, amr_id=None, **kwargs)
     :param converter: optional function to apply to inputs before evaluation
     :param amr_id: ID of AMR pair
     :param verbose: whether to print the results
-    :return: Scores object
+    :return: SmatchScores object
     """
-    def _read_amr(amr):
-        return "".join(l for l in (amr if isinstance(amr, list) else str(amr).splitlines()) if not l.startswith("#"))
     del kwargs
-    if converter is not None:
-        guessed = converter(guessed)
-        ref = converter(ref)
     smatch.verbose = verbose
-    guessed = _read_amr(guessed)
-    ref = _read_amr(ref)
-    try:
-        counts = smatch.process_amr_pair((guessed, ref, amr_id))
-    except (AttributeError, IndexError):  # error in one of the AMRs
-        try:
-            counts = smatch.process_amr_pair((ref, ref, amr_id))
-            counts = (0, 0, counts[-1])  # best_match_num, test_triple_num
-        except (AttributeError, IndexError):  # error in ref AMR
-            counts = (0, 0, 1)  # best_match_num, test_triple_num, gold_triple_num
-    return SmatchScores(counts)
+    a1, a2 = [read_amr(a, converter) for a in (guessed, ref)]
+    return SmatchScores((eval_type, get_scores(a1, a2, amr_id, eval_type, verbose)) for eval_type in EVAL_TYPES)
 
 
-class SmatchScores:
-    def __init__(self, counts):
-        self.counts = counts
-        self.precision, self.recall, self.f1 = smatch.compute_f(*counts)
+def read_amr(amr, converter=None):
+    if converter is not None:
+        amr = converter(amr)
+    return "".join(l for l in (amr if isinstance(amr, list) else str(amr).splitlines()) if not l.startswith("#"))
+
+
+class SmatchResults(evaluation.EvaluatorResults):
+    def __init__(self, *counts):
+        super().__init__({PRIMARY: evaluation.SummaryStatistics(*counts)}, default={PRIMARY.name: PRIMARY})
+        self.p, self.r, self.f1 = smatch.compute_f(*counts)
+
+
+class SmatchScores(evaluation.Scores):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
     @staticmethod
     def name():
@@ -49,34 +68,6 @@ class SmatchScores:
     @staticmethod
     def format():
         return "amr"
-
-    def average_f1(self, *args, **kwargs):
-        del args, kwargs  # TODO unlabeled smatch (delete all rels) when mode=UNLABELED
-        return self.f1
-
-    @staticmethod
-    def aggregate(scores):
-        """
-        Aggregate multiple Scores instances
-        :param scores: iterable of Scores
-        :return: new Scores with aggregated scores
-        """
-        return SmatchScores(map(sum, zip(*[s.counts for s in scores])))
-
-    def print(self, *args, **kwargs):
-        print("Smatch precision: %.3f\nSmatch recall: %.3f\nSmatch F1: %.3f\n" % (self.precision, self.recall, self.f1),
-              *args, **kwargs)
-
-    def fields(self):
-        return ["%.3f" % float(f) for f in (self.precision, self.recall, self.f1)]
-
-    def titles(self):
-        return self.field_titles()
-
-    @staticmethod
-    def field_titles(*args, **kwargs):
-        del args, kwargs
-        return ["precision", "recall", "f1"]
 
     def __str__(self):
         return ",".join(self.fields())
