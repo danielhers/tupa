04b6da118a3d94cc8e6ea3022ef3cc740f250003
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-02-07 12:36:04 +0200

    Create feature extractor from scratch for each comparison test
    Otherwise its data dict indices depend on test order.

--- tests/test_features.py
+++ tests/test_features.py
@@ -1,7 +1,5 @@
 """Testing code for the tupa.features package, unit-testing only."""
 
-from operator import itemgetter
-
 import pytest
 
 from tupa.action import Actions
@@ -10,22 +8,34 @@ from tupa.features.sparse_features import SparseFeatureExtractor
 from tupa.model import Model
 from tupa.oracle import Oracle
 from tupa.states.state import State
-from .conftest import load_passages, passage_id, config
+from .conftest import load_passages, passage_id
 
-PARAMS = {p.name: p.create_from_config() for p in Model(None, config=config()).param_defs}
+
+class FeatureExtractorCreator:
+    def __init__(self, name, indexed=False):
+        self.name = name
+        self.indexed = indexed
+
+    def __str__(self):
+        return self.name + ("-indexed" if self.indexed else "")
+
+    def __call__(self, config):
+        if self.name == "sparse":
+            return SparseFeatureExtractor()
+        return DenseFeatureExtractor({p.name: p.create_from_config() for p in Model(None, config=config).param_defs},
+                                     indexed=self.indexed)
+
 
 FEATURE_EXTRACTORS = [
-    ("sparse", SparseFeatureExtractor()),
-    ("dense", DenseFeatureExtractor(PARAMS, indexed=False)),
-    ("dense-indexed", DenseFeatureExtractor(PARAMS, indexed=True)),
+    FeatureExtractorCreator("sparse"), FeatureExtractorCreator("dense"), FeatureExtractorCreator("dense", indexed=True),
 ]
 
 
-@pytest.mark.parametrize("feature_extractor_item", FEATURE_EXTRACTORS, ids=itemgetter(0))
+@pytest.mark.parametrize("feature_extractor_creator", FEATURE_EXTRACTORS, ids=str)
 @pytest.mark.parametrize("passage", load_passages(), ids=passage_id)
-def test_features(config, feature_extractor_item, passage, write_features):
-    feature_extractor_id, feature_extractor = feature_extractor_item
+def test_features(config, feature_extractor_creator, passage, write_features):
     config.set_format(passage.extra.get("format"))
+    feature_extractor = feature_extractor_creator(config)
     oracle = Oracle(passage)
     state = State(passage)
     actions = Actions()
@@ -44,7 +54,7 @@ def test_features(config, feature_extractor_item, passage, write_features):
         if state.finished:
             break
     features = ["%s %s\n" % i for f in features if f for i in (sorted(f.items()) + [("", "")])]
-    compare_file = "test_files/features/%s-%s.txt" % (passage.ID, feature_extractor_id)
+    compare_file = "test_files/features/%s-%s.txt" % (passage.ID, feature_extractor_creator.name)
     if write_features:
         with open(compare_file, "w") as f:
             f.writelines(features)
@@ -52,14 +62,14 @@ def test_features(config, feature_extractor_item, passage, write_features):
         assert f.readlines() == features, compare_file
 
 
-@pytest.mark.parametrize("feature_extractor_item", FEATURE_EXTRACTORS[:-1], ids=itemgetter(0))
-def test_feature_templates(feature_extractor_item, write_features):
-    feature_extractor_id, feature_extractor = feature_extractor_item
+@pytest.mark.parametrize("feature_extractor_creator", FEATURE_EXTRACTORS[:-1], ids=str)
+def test_feature_templates(config, feature_extractor_creator, write_features):
+    feature_extractor = feature_extractor_creator(config)
     features = [feature_extractor.numeric_features_template if p.numeric else
                 feature_extractor.non_numeric_feature_templates[p.effective_suffix] for _, p in
                 sorted(feature_extractor.params.items())] or sorted(feature_extractor.feature_templates, key=str)
     features = ["%s\n" % i for i in features]
-    compare_file = "test_files/features/templates-%s.txt" % feature_extractor_id
+    compare_file = "test_files/features/templates-%s.txt" % feature_extractor_creator.name
     if write_features:
         with open(compare_file, "w") as f:
             f.writelines(features)
