6d13a4da206c32b7511836beb16d3652ff5e6c1c
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-02-15 11:00:25 +0200

    Fix missing numeric features when loading by order

--- tupa/features/feature_params.py
+++ tupa/features/feature_params.py
@@ -99,7 +99,7 @@ class FeatureParameters(Labels):
     @staticmethod
     def copy(params, copy_dict=dict, copy_init=True, order=None):
         return OrderedDict((suffix, params[suffix].copy_with_data(copy_dict, copy_init)) for suffix in
-                           (params if order is None else order) if suffix in params)
+                           (params if order is None else [NumericFeatureParameters.SUFFIX] + order) if suffix in params)
 
     def copy_with_data(self, copy_dict, copy_init):
         data = None if self.data is None else copy_dict(self.data)
