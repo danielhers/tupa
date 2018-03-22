97d00edec4adf7c18b96019db07a836848b2c6eb
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-03-22 10:04:08 +0200

    Script to set attrib["lang"] in passage files

---
+++ scheme/scripts/set_lang.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import argparse
+
+from tqdm import tqdm
+from ucca.ioutil import write_passage
+
+from scheme.scripts.annotate import add_specs_args, read_specs
+
+desc = """Read passages in any format, and write back with attrib['lang'] set."""
+
+
+def main(args):
+    for passages, out_dir, lang in read_specs(args):
+        for passage in tqdm(passages, unit=" passages", desc="Setting language in " + out_dir, postfix={"lang": lang}):
+            passage.attrib["lang"] = lang
+            write_passage(passage, outdir=out_dir, verbose=False, binary=args.binary)
+
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser(description=desc)
+    add_specs_args(argparser)
+    main(argparser.parse_args())
