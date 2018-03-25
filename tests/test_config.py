534a2b4a04918b5bb0826b25248fdd0e30d7182d
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-03-25 11:22:43 +0300

    Test nested fallback namespace

--- tests/test_config.py
+++ tests/test_config.py
@@ -25,11 +25,14 @@ def test_hyperparams(config):
     assert config.hyperparams.shared.layer_dim == 50, "--hyperparams=shared=--layer-dim=50"
     d = {"max_words_external": 100, "word_dim_external": 100, "optimizer": "sgd", "layer_dim": 100, "layers": 1}
     config.update(d)
-    config.update_hyperparams(shared={"lstm_layer_dim": 100, "lstm_layers": 1}, ucca={"word_dim": 300})
+    config.update_hyperparams(shared={"lstm_layer_dim": 100, "lstm_layers": 1}, ucca={"word_dim": 300},
+                              **{"ucca.en": {"dep_dim": 1}})
     assert config.hyperparams.shared.lstm_layer_dim == 100, "shared --lstm-layer-dim=100"
     assert config.hyperparams.shared.lstm_layers == 1, "shared --lstm-layers=1"
     assert config.hyperparams.shared.minibatch_size == 50, "--minibatch-size=50"
     assert config.hyperparams.specific["ucca"].word_dim == 300, "ucca --word-dim=300"
+    assert config.hyperparams.specific["ucca.en"].dep_dim == config.hyperparams.specific["ucca"]["en"].dep_dim == 1, \
+        "ucca.en --dep-dim=1"
     assert config.hyperparams.specific["ucca"].minibatch_size == 50, "--minibatch-size=50"
     for attr, value in d.items():
         assert getattr(config.hyperparams.shared, attr) == value, attr
