faf6604544e6893c3c648186f0426650c52330ac
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-05-17 09:26:02 +0300

    Fail oracle test on first action that doesn't match

--- tests/test_oracle.py
+++ tests/test_oracle.py
@@ -13,10 +13,22 @@ from .conftest import load_passages, Settings, passage_id
 def test_oracle(config, setting, passage, write_oracle_actions):
     config.update(setting.dict())
     config.set_format(passage.extra.get("format") or "ucca")
+    compare_file = "test_files/oracle_actions/%s%s.txt" % (passage.ID, setting.suffix())
+    actions_taken = []
+    with open(compare_file, "rw"[write_oracle_actions]) as f:
+        for i, action in enumerate(gen_actions(passage)):
+            if write_oracle_actions:
+                print(action, file=f)
+            else:
+                assert action == f.readline().strip(), "Action %d does not match expected (all actions taken: %s)" % (
+                    i, ", ".join(actions_taken) or "none")
+            actions_taken.append(action)
+
+
+def gen_actions(passage):
     oracle = Oracle(passage)
     state = State(passage)
     actions = Actions()
-    actions_taken = []
     while True:
         action = min(oracle.get_actions(state, actions).values(), key=str)
         state.transition(action)
@@ -25,12 +37,6 @@ def test_oracle(config, setting, passage, write_oracle_actions):
             label, _ = oracle.get_label(state, action)
             state.label_node(label)
             s += " " + str(label)
-        actions_taken.append(s + "\n")
+        yield s
         if state.finished:
             break
-    compare_file = "test_files/oracle_actions/%s%s.txt" % (passage.ID, setting.suffix())
-    if write_oracle_actions:
-        with open(compare_file, "w") as f:
-            f.writelines(actions_taken)
-    with open(compare_file) as f:
-        assert f.readlines() == actions_taken, compare_file
