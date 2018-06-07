3261030bfddd1f7d17f6fcf31eda25eea24870b8
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-06-07 10:54:01 +0300

    Refactor features test

--- tests/test_features.py
+++ tests/test_features.py
@@ -8,38 +8,50 @@ from tupa.action import Actions
 from tupa.features.dense_features import DenseFeatureExtractor
 from tupa.features.sparse_features import SparseFeatureExtractor
 from tupa.model import Model
-from tupa.model_util import load_enum
 from tupa.oracle import Oracle
 from tupa.states.state import State
 from .conftest import passage_files, load_passage, basename
 
+SPARSE = "sparse"
+DENSE = "dense"
+
 
 class FeatureExtractorCreator:
-    def __init__(self, name, indexed=False):
+    def __init__(self, name, indexed=False, annotated=False, vocab=None):
         self.name = name
         self.indexed = indexed
+        self.annotated = annotated
+        self.vocab = vocab
+        self.id = vocab == "-"
 
     def __str__(self):
-        return self.name + ("-indexed" if self.indexed else "")
+        return "-".join([self.name] + [attr for attr in ("indexed", "annotated", "vocab", "id") if getattr(self, attr)])
 
     def __call__(self, config):
-        if self.name == "sparse":
-            return SparseFeatureExtractor()
-        return DenseFeatureExtractor(OrderedDict((p.name, p.create_from_config())
-                                                 for p in Model(None, config=config).param_defs()),
-                                     indexed=self.indexed, node_dropout=0)
+        config.args.vocab = self.vocab
+        return SparseFeatureExtractor() if self.name == SPARSE else DenseFeatureExtractor(
+            OrderedDict((p.name, p.create_from_config()) for p in Model(None, config=config).param_defs()),
+            indexed=self.indexed, node_dropout=0)
 
 
-FEATURE_EXTRACTORS = [
-    FeatureExtractorCreator("sparse"), FeatureExtractorCreator("dense"), FeatureExtractorCreator("dense", indexed=True),
-]
+def all_feature_extractors(*args, **kwargs):
+    return [FeatureExtractorCreator(SPARSE, *args, **kwargs), FeatureExtractorCreator(DENSE, *args, **kwargs),
+            FeatureExtractorCreator(DENSE, *args, indexed=True, **kwargs)]
 
 
-def _test_features(config, feature_extractor_creator, filename, write_features, annotate=False, vocab=None):
-    passage = load_passage(filename, annotate=annotate)
-    textutil.annotate(passage, as_array=True, vocab=vocab and load_enum(vocab))
-    config.set_format(passage.extra.get("format") or "ucca")
+def extract_features(feature_extractor, state, features):
+    values = feature_extractor.extract_features(state)
+    if feature_extractor.params:
+        for key, vs in values.items():
+            assert len(vs) == feature_extractor.params[key].num, key
+    features.append(values)
+
+
+def _test_features(config, feature_extractor_creator, filename, write_features):
     feature_extractor = feature_extractor_creator(config)
+    passage = load_passage(filename, annotate=feature_extractor_creator.annotated)
+    textutil.annotate(passage, as_array=True, vocab=config.vocab())
+    config.set_format(passage.extra.get("format") or "ucca")
     oracle = Oracle(passage)
     state = State(passage)
     actions = Actions()
@@ -59,8 +71,7 @@ def _test_features(config, feature_extractor_creator, filename, write_features, 
         if state.finished:
             break
     features = ["%s %s\n" % i for f in features if f for i in (sorted(f.items()) + [("", "")])]
-    compare_file = "test_files/features/%s-%s%s%s.txt" % (basename(filename), str(feature_extractor_creator),
-                                                          "-annotated" if annotate else "", vocab or "")
+    compare_file = "test_files/features/%s-%s.txt" % (basename(filename), str(feature_extractor_creator))
     if write_features:
         with open(compare_file, "w") as f:
             f.writelines(features)
@@ -68,28 +79,19 @@ def _test_features(config, feature_extractor_creator, filename, write_features, 
         assert f.readlines() == features, compare_file
 
 
-def extract_features(feature_extractor, state, features):
-    values = feature_extractor.extract_features(state)
-    if feature_extractor.params:
-        for key, vs in values.items():
-            assert len(vs) == feature_extractor.params[key].num, key
-    features.append(values)
-
-
-@pytest.mark.parametrize("feature_extractor_creator", FEATURE_EXTRACTORS, ids=str)
+@pytest.mark.parametrize("feature_extractor_creator", all_feature_extractors(), ids=str)
 @pytest.mark.parametrize("filename", passage_files(), ids=basename)
 def test_features(config, feature_extractor_creator, filename, write_features):
     _test_features(config, feature_extractor_creator, filename, write_features)
 
 
-@pytest.mark.parametrize("feature_extractor_creator", FEATURE_EXTRACTORS, ids=str)
+@pytest.mark.parametrize("feature_extractor_creator", all_feature_extractors(annotated=True, vocab="-"), ids=str)
 @pytest.mark.parametrize("filename", passage_files("conllu"), ids=basename)
-@pytest.mark.parametrize("vocab", ("-", None), ids=("vocab", ""))
-def test_features_annotate(config, feature_extractor_creator, filename, vocab, write_features):
-    _test_features(config, feature_extractor_creator, filename, write_features, annotate=True, vocab=vocab)
+def test_features_annotated(config, feature_extractor_creator, filename, write_features):
+    _test_features(config, feature_extractor_creator, filename, write_features)
 
 
-@pytest.mark.parametrize("feature_extractor_creator", FEATURE_EXTRACTORS[:-1], ids=str)
+@pytest.mark.parametrize("feature_extractor_creator", all_feature_extractors()[:-1], ids=str)
 def test_feature_templates(config, feature_extractor_creator, write_features):
     config.set_format("amr")
     feature_extractor = feature_extractor_creator(config)
