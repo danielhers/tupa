63c8c6df4b07fc95dedb41270c0d70cb6bfa4dac
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-08-09 20:22:14 +0300

    Fix split command

--- ci/test.sh
+++ ci/test.sh
@@ -20,7 +20,7 @@ if ! [[ "$ACTION" =~ ^(toy|unit)$ ]]; then
     amr)
         curl -L --remote-name-all https://amr.isi.edu/download/2016-03-14/alignment-release-{training,dev,test}-bio.txt
         rename 's/.txt/.amr/' alignment-release-*-bio.txt
-        python -m semstr.scripts.split -q alignment-release-training-bio.amr alignment-release-training-bio
+        python -m semstr.scripts.split -q alignment-release-training-bio.amr -o alignment-release-training-bio
         CONVERT_DATA=alignment-release-dev-bio.amr
         TRAIN_DATA=alignment-release-training-bio
         DEV_DATA=alignment-release-dev-bio.amr
@@ -28,7 +28,7 @@ if ! [[ "$ACTION" =~ ^(toy|unit)$ ]]; then
     sdp)
         mkdir data
         curl -L http://svn.delph-in.net/sdp/public/2015/trial/current.tgz | tar xz -C data
-        python -m semstr.scripts.split -q data/sdp/trial/dm.sdp data/sdp/trial/dm
+        python -m semstr.scripts.split -q data/sdp/trial/dm.sdp -o data/sdp/trial/dm
         python -m scripts.split_corpus -q data/sdp/trial/dm -t 120 -d 36 -l
         CONVERT_DATA=data/sdp/trial/*.sdp
         TRAIN_DATA=data/sdp/trial/dm/train
