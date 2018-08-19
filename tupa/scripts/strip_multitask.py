fd5039826bbd953ac9c522821fec45f617eeab14
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-08-19 14:16:33 +0300

    Better documentation in strip_multitask

--- tupa/scripts/strip_multitask.py
+++ tupa/scripts/strip_multitask.py
@@ -1,22 +1,22 @@
 import os
 from configargparse import ArgParser
 
+from tupa.config import FORMATS
 from tupa.model import NODE_LABEL_KEY
 from tupa.scripts.export import load_model
 
 
 def strip_multitask(model, keep):
-    if "amr" not in keep:
-        for d in (model.feature_params, model.classifier.params):
-            for key in (NODE_LABEL_KEY, "c"):
-                try:
-                    del d[key]
-                except KeyError:
-                    pass
-    for d in (model.classifier.labels, model.classifier.axes):
-        for axis in set(d).difference(keep):
+    if "amr" not in keep:  # Remove AMR-specific features: node label and category
+        delete_if_exists((model.feature_params, model.classifier.params), (NODE_LABEL_KEY, "c"))
+    delete_if_exists((model.classifier.labels, model.classifier.axes), {NODE_LABEL_KEY}.union(FORMATS).difference(keep))
+
+
+def delete_if_exists(dicts, keys):
+    for d in dicts:
+        for key in keys:
             try:
-                del d[axis]
+                del d[key]
             except KeyError:
                 pass
 
@@ -33,6 +33,7 @@ def main(args):
 if __name__ == "__main__":
     argparser = ArgParser(description="Load TUPA model and save with just one task's features/weights.")
     argparser.add_argument("models", nargs="+", help="model file basename(s) to load")
-    argparser.add_argument("-k", "--keep", nargs="+", default=["ucca"], help="tasks to keep features/weights for")
-    argparser.add_argument("-o", "--out-dir", default=".", help="filename suffix to append")
+    argparser.add_argument("-k", "--keep", nargs="+", choices=tuple(filter(None, FORMATS)), default=["ucca"],
+                           help="tasks to keep features/weights for")
+    argparser.add_argument("-o", "--out-dir", default=".", help="directory to write modified model files to")
     main(argparser.parse_args())
