71bf08d0ffd956b9ec4911e5d1b3f5dd746f4c6c
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-03-01 16:36:39 +0100

    Fix depth to actually be > 0...

--- tupa/scripts/annotate_structural.py
+++ tupa/scripts/annotate_structural.py
@@ -14,6 +14,7 @@ def get_depth(unit):
     while unit.parents:
         try:
             unit = parent(unit)
+            depth += 1
         except StopIteration:
             return -1
     return depth
