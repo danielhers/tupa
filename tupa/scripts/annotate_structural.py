90debecb181b6ca185332ea8227d7a231dca49a1
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-03-01 16:56:41 +0100

    Add next_terminal_lower_common_ancestor_category to annotation

--- tupa/scripts/annotate_structural.py
+++ tupa/scripts/annotate_structural.py
@@ -20,24 +20,35 @@ def get_depth(unit):
     return depth
 
 
-def get_next_terminal_lower_common_ancestor_depth(terminal):
+def get_next_terminal_lower_common_ancestor(terminal):
     try:
         next_terminal = terminal.layer.by_position(terminal.position + 1)
     except IndexError:
-        return 0
+        return None
     unit = terminal
     while unit.parents and next_terminal not in unit.iter():
         try:
             unit = parent(unit)
         except StopIteration:
-            return -1
-    return get_depth(unit)
+            return None
+    return unit
+
+
+def get_next_terminal_lower_common_ancestor_depth(terminal):
+    unit = get_next_terminal_lower_common_ancestor(terminal)
+    return get_depth(unit) if unit else 0
+
+
+def get_next_terminal_lower_common_ancestor_category(terminal):
+    unit = get_next_terminal_lower_common_ancestor(terminal)
+    return getattr(unit, "ftag", None) if unit else None
 
 
 def get_features(terminal):
     return dict(
         depth=get_depth(terminal),
         next_terminal_lower_common_ancestor_depth=get_next_terminal_lower_common_ancestor_depth(terminal),
+        next_terminal_lower_common_ancestor_category=get_next_terminal_lower_common_ancestor_category(terminal),
     )
 
 
@@ -52,8 +63,8 @@ def hack_id(sent_id):
 def main(args):
     os.makedirs(args.out_dir, exist_ok=True)
     features_passage_by_id = {hack_id(passage.ID): passage for passage in get_passages_with_progress_bar(
-        args.features_source_dir, desc="Reading passages for structure features")}
-    for passage in get_passages_with_progress_bar(args.source_dir, desc="Reading source passages"):
+        args.features_source_dir, desc="Reading passages for features")}
+    for passage in get_passages_with_progress_bar(args.source_dir, desc="Annotating"):
         try:
             features_passage = features_passage_by_id[passage.ID]
         except KeyError as e:
