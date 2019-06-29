79f872ce79af132d0b28968dfb39b69048a5403b
Ofir Arviv <t-ofarvi@microsoft.com>
2019-06-29 19:02:52 +0300

    always assert lang in xml

--- tupa/parse.py
+++ tupa/parse.py
@@ -73,11 +73,8 @@ class PassageParser(AbstractParser):
             assert not errors, errors
         self.in_format = self.format or "ucca"
         self.out_format = "ucca" if self.format in (None, "text") else self.format
-        if self.config.args.bert_multilingual is not None:
-            self.lang = self.passage.attrib.get("lang")
-            assert self.lang
-        else:
-            self.lang = self.passage.attrib.get("lang", self.config.args.lang)
+        self.lang = self.passage.attrib.get("lang")
+        assert self.lang
         # Used in verify_passage to optionally ignore a mismatch in linkage nodes:
         self.ignore_node = None if self.config.args.linkage else lambda n: n.tag == layer1.NodeTags.Linkage
         self.state_hash_history = set()
