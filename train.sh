172ae405ef6a116b8b356b5c9637b69dbee871ae
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-07-16 17:46:17 +0200

    Use GPU for BERT in training and parsing

--- train.sh
+++ train.sh
@@ -25,6 +25,6 @@ TOTAL=`cat models/mrp-${SUFFIX}.train_dev.mrp | wc -l`
 head -n$((TOTAL * 95 / 100)) models/mrp-${SUFFIX}.train_dev.mrp > models/mrp-${SUFFIX}.train.mrp
 head -n$((TOTAL * 5 / 100)) models/mrp-${SUFFIX}.train_dev.mrp > models/mrp-${SUFFIX}.dev.mrp
 
-python -m tupa --seed $RANDOM --cores=15 --use-bert --dynet-gpu --no-validate-oracle \
+python -m tupa --seed $RANDOM --cores=15 --use-bert --pytorch-gpu --no-validate-oracle \
     -t models/mrp-${SUFFIX}.train.mrp -d models/mrp-${SUFFIX}.dev.mrp \
     --conllu ../mrp/2019/companion/udpipe.mrp --alignment ../mrp/2019/companion/isi.mrp -m models/mrp-${SUFFIX}
