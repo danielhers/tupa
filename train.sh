5ef66598f3c659260c89927e897125331352459b
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-07-16 15:47:49 +0200

    Support training on just one framework in script

--- train.sh
+++ train.sh
@@ -9,11 +9,21 @@ if [[ $# -lt 1 ]]; then
 else
     SUFFIX="$1"
 fi
+if [[ -n "${FRAMEWORK}" ]]; then
+    SUFFIX=${FRAMEWORK}-${SUFFIX}
+fi
 
 shuf ../mrp/2019/training/training.mrp > models/mrp-${SUFFIX}.train_dev.mrp
+
+if [[ -n "${FRAMEWORK}" ]]; then
+    grep '"framework": "'${FRAMEWORK}'"' models/mrp-${SUFFIX}.train_dev.mrp > models/mrp-${SUFFIX}.train_dev.mrp.temp
+    mv models/mrp-${SUFFIX}.train_dev.mrp.temp models/mrp-${SUFFIX}.train_dev.mrp
+fi
+
 TOTAL=`cat models/mrp-${SUFFIX}.train_dev.mrp | wc -l`
 head -n$((TOTAL * 95 / 100)) models/mrp-${SUFFIX}.train_dev.mrp > models/mrp-${SUFFIX}.train.mrp
 head -n$((TOTAL * 5 / 100)) models/mrp-${SUFFIX}.train_dev.mrp > models/mrp-${SUFFIX}.dev.mrp
+
 python -m tupa --seed $RANDOM --cores=15 --use-bert --dynet-gpu \
     -t models/mrp-${SUFFIX}.train.mrp -d models/mrp-${SUFFIX}.dev.mrp \
     --conllu ../mrp/2019/companion/udpipe.mrp --alignment ../mrp/2019/companion/isi.mrp -m models/mrp-${SUFFIX}
