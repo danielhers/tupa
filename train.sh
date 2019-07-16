653a8c30ea493a2c0853df34eb613e493d91b18e
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-07-16 15:54:06 +0200

    Do not validate oracle when training

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#SBATCH --mem=40G
+#SBATCH --mem=50G
 #SBATCH --time=7-0
 #SBATCH --gres=gpu:1
 #SBATCH -c16
@@ -24,6 +24,6 @@ TOTAL=`cat models/mrp-${SUFFIX}.train_dev.mrp | wc -l`
 head -n$((TOTAL * 95 / 100)) models/mrp-${SUFFIX}.train_dev.mrp > models/mrp-${SUFFIX}.train.mrp
 head -n$((TOTAL * 5 / 100)) models/mrp-${SUFFIX}.train_dev.mrp > models/mrp-${SUFFIX}.dev.mrp
 
-python -m tupa --seed $RANDOM --cores=15 --use-bert --dynet-gpu \
+python -m tupa --seed $RANDOM --cores=15 --use-bert --dynet-gpu --no-validate-oracle \
     -t models/mrp-${SUFFIX}.train.mrp -d models/mrp-${SUFFIX}.dev.mrp \
     --conllu ../mrp/2019/companion/udpipe.mrp --alignment ../mrp/2019/companion/isi.mrp -m models/mrp-${SUFFIX}
