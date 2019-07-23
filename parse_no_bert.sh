0df2af5fb477f3ffdaa3f469cfdabbed8ea50d31
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-07-23 14:33:42 +0200

    Script to parse with a non-BERT model

--- parse_no_bert.sh
+++ parse_no_bert.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 #SBATCH --mem=50G
-#SBATCH --time=1-0
+#SBATCH --time=7-0
 #SBATCH --gres=gpu:1
+#SBATCH -c16
 
 if [[ $# -lt 1 ]]; then
-    echo "Required: model suffix"
-    exit 1
+    SUFFIX=`date '+%Y%m%d'`
 else
     SUFFIX="$1"
 fi
 
 python -m tupa --use-bert --dynet-gpu --pytorch-gpu \
-    ../mrp/2019/evaluation/input.mrp models/mrp-${SUFFIX}.output.mrp \
+    ../mrp/2019/evaluation/input.mrp \
     --conllu ../mrp/2019/evaluation/udpipe.mrp --alignment ../mrp/2019/evaluation/isi.mrp -m models/mrp-${SUFFIX} -v
