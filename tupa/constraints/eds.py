df9bcb4b832c3be1a25bf54ebf332caa0d46e9d6
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-07-27 16:31:18 +0200

    Ensure up to 1 anchor per node in EDS

--- tupa/constraints/eds.py
+++ tupa/constraints/eds.py
@@ -1,6 +1,6 @@
-from .validation import Constraints
+from .validation import Constraints, ANCHOR_LAB
 
 
 class EdsConstraints(Constraints):
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+        super().__init__(**kwargs, unique_outgoing=ANCHOR_LAB)
