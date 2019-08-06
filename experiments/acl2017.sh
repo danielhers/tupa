ce25aa18c07e64afc2bff96821d7fe59f51f7145
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-08-06 09:46:26 +0200

    Split ACL 2017 job on CI

--- experiments/acl2017.sh
+++ experiments/acl2017.sh
@@ -18,16 +18,22 @@ fi
 pip install "tupa>=1.0,<1.1"
 git clone https://github.com/huji-nlp/ucca-corpora --branch v1.2
 mkdir -p models wiki-sentences 20k-sentences
-python -m scripts.standard_to_sentences ucca-corpora/wiki/xml/*.xml -o wiki-sentences
-python -m scripts.standard_to_sentences ucca-corpora/vmlslm/en/*.xml -o 20k-sentences
-python -m scripts.split_corpus wiki-sentences -t 4268 -d 454 -l
+if [[ -z "$CORPUS" || "$CORPUS" == wiki ]]; then
+    python -m scripts.standard_to_sentences ucca-corpora/wiki/xml/*.xml -o wiki-sentences
+    python -m scripts.split_corpus wiki-sentences -t 4268 -d 454 -l
+fi
+if [[ -z "$CORPUS" || "$CORPUS" == 20k ]]; then
+    python -m scripts.standard_to_sentences ucca-corpora/vmlslm/en/*.xml -o 20k-sentences
+fi
 curl -L --remote-name-all https://github.com/huji-nlp/tupa/releases/download/v1.0/{sparse,mlp,bilstm}.tgz
 tar xvzf sparse.tgz
 tar xvzf mlp.tgz
 tar xvzf bilstm.tgz
 python -m spacy download en
 for TEST_SET in wiki-sentences/test 20k-sentences; do
-    for MODEL in sparse mlp bilstm; do
-        python -m tupa.parse -c $MODEL -m models/$MODEL -We $TEST_SET
-    done
+    if [[ -z "$CORPUS" || $TEST_SET == "$CORPUS"* ]]; then
+        for MODEL in sparse mlp bilstm; do
+            python -m tupa.parse -c $MODEL -m models/$MODEL -We $TEST_SET
+        done
+    fi
 done
