97bf5bd3a2eb91d926725123ac5ae978d3ca79cb
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-08-06 10:14:59 +0200

    Split ACL 2018 job on CI

--- experiments/acl2018.sh
+++ experiments/acl2018.sh
@@ -18,48 +18,57 @@ fi
 pip install ucca==1.0.67 tupa==1.3.2
 git clone https://github.com/huji-nlp/ucca-corpora --branch v1.2
 mkdir -p models wiki-sentences 20k{,-fr,-de}-sentences
-if [[ -z "$CORPUS" || "$CORPUS" == wiki ]]; then
+if [[ -z ${CORPUS+x} || "$CORPUS" == wiki ]]; then
     python -m scripts.standard_to_sentences ucca-corpora/wiki/xml -o wiki-sentences
     python -m scripts.split_corpus wiki-sentences -q -t 4268 -d 454 -l
 fi
-if [[ -z "$CORPUS" || "$CORPUS" == 20k ]]; then
+if [[ -z ${CORPUS+x} || "$CORPUS" == 20k ]]; then
     python -m scripts.standard_to_sentences ucca-corpora/vmlslm/en -o 20k-sentences
 fi
-if [[ -z "$CORPUS" || "$CORPUS" == wiki || "$CORPUS" == 20k ]]; then
-    curl -L --remote-name-all https://github.com/huji-nlp/tupa/releases/download/v1.3.2/ucca{,-amr,-dm,-ud.,-amr-dm,-amr-ud.,-amr-dm-ud.}-bilstm-1.3.2.tar.gz
-    tar xvzf ucca-bilstm-1.3.2.tar.gz
-    tar xvzf ucca-amr-bilstm-1.3.2.tar.gz
-    tar xvzf ucca-dm-bilstm-1.3.2.tar.gz
-    tar xvzf ucca-ud.-bilstm-1.3.2.tar.gz
-    tar xvzf ucca-amr-dm-bilstm-1.3.2.tar.gz
-    tar xvzf ucca-amr-ud.-bilstm-1.3.2.tar.gz
-    tar xvzf ucca-amr-dm-ud.-bilstm-1.3.2.tar.gz
+if [[ -z ${CORPUS+x} || "$CORPUS" == wiki || "$CORPUS" == 20k ]]; then
+      for AUX in "" -amr -dm -ud++ -amr-dm -amr-ud++ -amr-dm-ud++; do
+          if [[ -z ${MODEL+x} || $AUX == "$MODEL" ]]; then
+              curl -L --remote-name-all https://github.com/huji-nlp/tupa/releases/download/v1.3.2/ucca${AUX/++/.}-bilstm-1.3.2.tar.gz
+              tar xvzf ucca${AUX/++/.}-bilstm-1.3.2.tar.gz
+          fi
+    done
 fi
-if [[ -z "$CORPUS" || "$CORPUS" == fr ]]; then
+if [[ -z ${CORPUS+x} || "$CORPUS" == fr ]]; then
     python -m scripts.standard_to_sentences ucca-corpora/vmlslm/fr -o 20k-fr-sentences
     python -m scripts.split_corpus 20k-fr-sentences -q -t 413 -d 67 -l
-    curl -L --remote-name-all https://github.com/huji-nlp/tupa/releases/download/v1.3.2/ucca{,-ud}-bilstm-1.3.2-fr.tar.gz
-    tar xvzf ucca-bilstm-1.3.2-fr.tar.gz
-    tar xvzf ucca-ud-bilstm-1.3.2-fr.tar.gz
+    for AUX in "" -ud; do
+        if [[ -z ${MODEL+x} || $AUX == "$MODEL" ]]; then
+            curl -L --remote-name-all https://github.com/huji-nlp/tupa/releases/download/v1.3.2/ucca${AUX}-bilstm-1.3.2-fr.tar.gz
+            tar xvzf ucca${AUX}-bilstm-1.3.2-fr.tar.gz
+        fi
+    done
 fi
-if [[ -z "$CORPUS" || "$CORPUS" == de ]]; then
+if [[ -z ${CORPUS+x} || "$CORPUS" == de ]]; then
     python -m scripts.standard_to_sentences ucca-corpora/20k_de/xml -o 20k-de-sentences
     python -m scripts.split_corpus 20k-de-sentences -q -t 3429 -d 561 -l
-    curl -L --remote-name-all https://github.com/huji-nlp/tupa/releases/download/v1.3.2/ucca{,-ud}-bilstm-1.3.2-de.tar.gz
-    tar xvzf ucca-bilstm-1.3.2-de.tar.gz
-    tar xvzf ucca-ud-bilstm-1.3.2-de.tar.gz
+    for AUX in "" -ud; do
+        if [[ -z ${MODEL+x} || $AUX == "$MODEL" ]]; then
+            curl -L --remote-name-all https://github.com/huji-nlp/tupa/releases/download/v1.3.2/ucca${AUX}-bilstm-1.3.2-de.tar.gz
+            tar xvzf ucca${AUX}-bilstm-1.3.2-de.tar.gz
+        fi
+    done
 fi
 export SPACY_MODEL_EN=en_core_web_lg
 for TEST_SET in wiki-sentences/test 20k-sentences; do
-    if [[ -z "$CORPUS" || $TEST_SET == "$CORPUS"* ]]; then
+    if [[ -z ${CORPUS+x} || $TEST_SET == "$CORPUS"* ]]; then
         for AUX in "" -amr -dm -ud++ -amr-dm -amr-ud++ -amr-dm-ud++; do
-            python -m tupa -m models/ucca$AUX-bilstm -We $TEST_SET
+            if [[ -z ${MODEL+x} || $AUX == "$MODEL" ]]; then
+                python -m tupa -m models/ucca$AUX-bilstm -We $TEST_SET
+            fi
         done
     fi
 done
 for LANG in fr de; do
-    if [[ -z "$CORPUS" || $LANG == "$CORPUS" ]]; then
-        python -m tupa -m models/ucca-bilstm-$LANG -We 20k-$LANG-sentences/test --lang $LANG
-        python -m tupa -m models/ucca-ud-bilstm-$LANG -We 20k-$LANG-sentences/test --lang $LANG
+    if [[ -z ${CORPUS+x} || $LANG == "$CORPUS" ]]; then
+        for AUX in "" -ud; do
+            if [[ -z ${MODEL+x} || $AUX == "$MODEL" ]]; then
+                python -m tupa -m models/ucca${AUX}-bilstm-$LANG -We 20k-$LANG-sentences/test --lang $LANG
+            fi
+        done
     fi
 done
